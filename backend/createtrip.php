637697f5ae87f575d74f0c937c366376868e46a0
Niels Elgaard Larsen <elgaard@agol.dk>
2014-12-05 04:35:29 +0100

    backend api implemtation of trip creation still need consistency checks.

---
+++ backend/createtrip.php
@@ -0,0 +1,38 @@
+<?php
+ini_set('default_charset', 'utf-8');
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+$season=date('Y');
+if(!isset($_SESSION))  session_start();
+$rodb=new mysqli("localhost","roprotokol","","roprotokol");
+
+error_log('create trip ss'.$season);
+if ($rodb->connect_errno) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+ 
+if (!$rodb->set_charset("utf8")) {
+  printf("Error loading character set utf8: %s\n", $rodb->error);
+}
+
+$data = file_get_contents("php://input");
+$newtrip=json_decode($data);
+if ($stmt = $rodb->prepare("INSERT INTO Trip(Season,BoatID,Destination,TripTypeID,CreatedDate,EditDate,OutTime,ExpectedIn) VALUES(?,?,?,?,NOW(),NOW(),?,?)")) { 
+     $stmt->bind_param('iisiss', $season, $newtrip->boat->id , $newtrip->destination->name, $newtrip->triptype->id, $newtrip->starttime, $newtrip->expectedtime);
+     error_log('now EXE');
+     $stmt->execute(); 
+} 
+
+ if ($stmt = $rodb->prepare("INSERT INTO TripMember(TripID,Season,Seat,MemberID,MemberName,CreatedDate,EditDate)  VALUES(LAST_INSERT_ID(),?,?,?,?,NOW(),NOW())")) {
+     $seat=1;
+     foreach ($newtrip->rowers as $rower) {
+         $stmt->bind_param('iiis',$season,$seat, $rower->id,$rower->name);
+         $stmt->execute();
+         $seat+=1;
+     }
+ } 
+
+$rodb->close();
+?> 
