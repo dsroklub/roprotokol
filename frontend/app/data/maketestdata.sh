c770dc139fc9a2eb5785655c71004b2c6174462d
Niels Elgaard Larsen <elgaard@agol.dk>
2014-12-14 22:14:56 +0100

    preserve utf in prettyfying

--- frontend/app/data/maketestdata.sh
+++ frontend/app/data/maketestdata.sh
@@ -1,3 +1,3 @@
 for un in boatdamages  boatreservations  boats  boatstatus  destinations  rostat  rowers  rower_statistics  triptypes; do
-    wget -O - http://localhost/DSR-roprotokol/backend/$un.php|json_pp > $un.json
+    wget -O - http://localhost/DSR-roprotokol/backend/$un.php|json_pp -json_opt pretty,utf8 > $un.json
     done
