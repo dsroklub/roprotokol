6d9e487a7583f1e7d514e776a3ba4a1c1930f0b2
Niels Elgaard Larsen <elgaard@agol.dk>
2014-12-25 18:57:50 +0100

    updated data

--- frontend/app/data/maketestdata.sh
+++ frontend/app/data/maketestdata.sh
@@ -1,4 +1,4 @@
-for un in boatdamages.php  boatreservations.php  boats.php  boatstatus.php  destinations.php  rostat.php  rowers.php  boat_statistics.php boat_statistics.php?boattype=kayak boat_statistics.php?boattype=rowboat  rower_statistics.php?boattype=kayak rower_statistics.php?boattype=rowboat rower_statistics.php  triptypes.php; do
+for un in boatdamages.php  boatreservations.php  boats.php  boatstatus.php  destinations.php  rowers.php  boat_statistics.php boat_statistics.php?boattype=kayak boat_statistics.php?boattype=rowboat  rower_statistics.php?boattype=kayak rower_statistics.php?boattype=rowboat rower_statistics.php  triptypes.php; do
     uns=${un/\?/Q}
     une=${uns/=/}
     wget -O - "http://localhost/DSR-roprotokol/backend/$un"|json_pp -json_opt pretty,utf8 > "${une/.php/}.json"
