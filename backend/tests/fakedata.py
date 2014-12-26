4a4ad2f47a2bd5182590ebf51a8b33a90e907def
Niels Elgaard Larsen <elgaard@agol.dk>
2014-12-26 20:05:09 +0100

    do fake destinations

--- backend/tests/fakedata.py
+++ backend/tests/fakedata.py
@@ -8,6 +8,7 @@ import sys
 
 numrowers=1000
 
+
 random.seed(42)
 # Password is read from file sectret.db
 # but do not write it in this file
@@ -22,6 +23,9 @@ else:
 cur = db.cursor() 
 
 #cur.execute("SELECT * FROM Medlem")
+
+destinations=["No dest","Bellevue","Charlottenlund","Flakfortet","Hellerup","Kanalen","Knud","Langelinie","Margretheholms havn","Opfyldningen nord","Rungsted","Skodsborg","Skovshoved","Slusen","Strandmøllen",
+              "Svanemøllehavnen","Tuborg havn","Tårbæk","Vedbæk","Langt Væk","London"];
 
 fnames=["Agnes","Agnete","Alberte","Amalie","Amanda","Andrea","Ane","Anette","Anna","Anne","Annemette","Annette","Asta","Astrid","Benedikte","Bente","Benthe","Berit","Beth","Bettina","Birgit","Birgitte","Birte","Birthe","Bodil","Britta","Camilla","Carina","Caroline","Catrine","Cecilie","Charlotte","Clara","Connie","Conny","Dagmar","Dagny","Daniella","Dina","Ditte","Doris","Dorte","Dorthe","Edith","Elin","Elisabeth","Ella","Ellen","Elna","Else","Elsebeth","Emilie","Emily","Emma","Erna","Esmarelda","Ester","Filippa","Frederikke","Freja","Frida","Gerda","Gertrud","Gitte","Grete","Grethe","Gundhild","Gunhild","Gurli","Hanne","Heidi","Helen","Helle","Henriette","Herdis","Iben","Ida","Inge","Ingelise","Inger","Ingrid","Irma","Isabella","Janne","Janni","Jannie","Jasmin","Jenny","Joan","Johanne","Jonna","Josefine","Josephine","Julie","Jytte","Karen","Karin","Karina","Karla","Karoline","Katcha","Katja","Katrine","Kirsten","Kirstin","Kirstine","Klara","Kristina","Kristine","Laura","Lea","Lena","Lene","Leonora","Line","Liva","Lona","Lone","Lotte","Louise","Lærke","Maiken","Maja","Majken","Malene","Malou","Maren","Margit","Margrethe","Maria","Marianne","Marie","Marlene","Mathilde","Maya","Merete","Merethe","Mette","Mia","Michala","Michelle","Mie","Mille","Mimi","Minna","Nadia","Naja","Nana","Nanna","Nanni","Natasha","Natasja","Nete","Nicoline","Nina","Nora","Ofelia","Olga","Olivia","Patricia","Paula","paulina","Pernille","Pia","Ragna","Ragnhild","Randi","Rebecca","Regitze","Rikke","Rita","Ritt","Ronja","Rosa","Ruth","Sabine","Sandra","Sanne","Sara","Sarah","Selma","Signe","Sigrid","Sille","Simone","Sine","Sofia","Sofie","Solveig","Solvej","Sonja","Sophie","Stina","Stine","Susanne","Sussanne","Sussie","Sys","Sørine","Søs","Tammy","Tanja","Thea","Tilde","Tina","Tine","Tove","Trine","Ulla","Ulrike","Ursula","Vera","Victoria","Viola","Vivian","Winnie","Xenia","Yasmin","Yda","Yrsa","Yvonne","Zahra","Zara","Åse","Adam","Albert","Aksel","Alex","Alexander","Alf","Allan","Alvin","Anders","André","Andreas","Anton","Arne","Asger","August","Benjamin","Benny","Bent","Bertil","Bertram","Birger","Bjarne","Bo","Bob","Bobby","Boe","Boris","Borris","Brian","Bruno","Bøje","Børge","Carl","Carlo","Carsten","Casper","Christen","Christian","Christoffer","Christopher","Claus","Clavs","Curt","Dan","Daniel","Danny","Dave","David","Dennis","Ebbe","Einar","Einer","Elias","Emil","Eric","Erik","Erling","Ernst","Esben","Finn","Flemming","Frank","Frans","Freddy","Frede","Frederik","Frode","Georg","George","Gert","Gorm","Gunnar","Gunner","Gustav","Hans","Helge","Henrik","Henry","Herbert","Herman","Hjalte","Holger","Hugo","Ib","Ivan","Iver","Jack","Jacob","Jakob","James","Jan","Jano","Jarl","Jean","Jens","Jeppe","Jesper","Jim","Jimmy","Joachim","Joakim","Johan","Johannes","John","Johnnie","Johnny","Jon","Jonas","Jonathan","Julius","Jørgen","Karl","Karlo","Karsten","Kaspar","Kasper","Keld","Ken","Kenn","Kenneth","Kenny","Kent","Kim","Kjeld","Klaus","Klavs","Kristian","Kurt","Kåre","Lars","Lasse","Laurits","Laus","Laust","Leif","Lennarth","Lucas","Ludvig","Mads","Magnus","Malthe","Marcus","Marius","Mark","Martin","Mathias","Matthias","Michael","Mik","Mikael","Mike","Mikkel","Mogens","Morten","Nick","Nicklas","Niels","Nikolai","Nils","Noah","Ole","Olfert","Oliver","Oscar","Oskar","Osvald","Otto","Ove","Palle","Patrick","Paw","Peder","Per","Pete","Peter","Paul","Philip","Poul","Preben","Ragnar","Rasmus","Richard","Richardt","Robert","Robin","Rolf","Ron","Ronni","Ronnie","Rune","Sebastian","Silas","Simon","Steen","Stefan","Sten","Stig","Svenning","Søren","Tage","Thomas","Tim","Timmy","Tobias","Tom","Tommy","Tonny","Torben","Troels","Uffe","Ulf","Ulrik","Vagn","Valdemar","Verner","Victor","Villads","Werner","William","Yngve","Zacharias","Ziggy","Øivind","Øjvind","Øyvind","Aage","Åge"]
 
@@ -79,8 +83,7 @@ for tid in range(1, 4000):
     pladser=int(boat[1])
     destination=random.randrange(1, 19)
     triptype=random.randrange(1, 12)
-
-    q="INSERT INTO Trip (TripID, Season, BoatID,Destination,Meter,TripTypeID,DESTID, OutTime, intime) VALUES ("+str(tid)+',2014,'+str(bid)+',"et eller andet sted",'+str(random.randrange(500,50000))+','+str(triptype)+','+str(destination)+',"2014-05-14 02:02:03", '+str(intime)+')';
+    q="INSERT INTO Trip (TripID, Season, BoatID,Destination,Meter,TripTypeID,DESTID, OutTime, intime) VALUES ("+str(tid)+',2014,'+str(bid)+',"'+str(destinations[destination])+'",'+str(random.randrange(500,50000))+','+str(triptype)+','+str(destination)+',"2014-05-14 02:02:03", '+str(intime)+')';
     print q
     cur.execute(q);
     for d in range(1,pladser+1):
