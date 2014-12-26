5333775e24e9e6013c9a6620e399280b338c1d38
Niels Elgaard Larsen <elgaard@agol.dk>
2014-12-26 20:06:02 +0100

    whitespace formatting

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -13,29 +13,29 @@ var app = angular.module('myApp', [
   'myApp.database',
   'myApp.utilities'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/boat/checkout/:boat_id', {
-    templateUrl: 'templates/boat/checkout.html',
-    controller: 'BoatCtrl'
-  });
-  $routeProvider.when('/categoryoverview/', {
-    templateUrl: 'templates/boat/categoryoverview.html',
-    controller: 'BoatCtrl'
-  });
-  $routeProvider.when('/boat/categoryoverview/', {
-    templateUrl: 'templates/boat/categoryoverview.html',
-    controller: 'BoatCtrl'
-  });
-  $routeProvider.when('/statoverview/', {
-    templateUrl: 'templates/stats/statoverview.html',
-    controller: 'StatCtrl'
-  });
-  $routeProvider.when('/', {redirectTo: '/boat/categoryoverview'});
-  $routeProvider.otherwise({
-    templateUrl: 'templates/notimplementet.html',
-  });
-}])
-.config(['uiSelectConfig', function(uiSelectConfig) {
-  uiSelectConfig.theme = 'bootstrap';
-}]);
-
+    config([
+      '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/boat/checkout/:boat_id', {
+	  templateUrl: 'templates/boat/checkout.html',
+	  controller: 'BoatCtrl'
+	});
+	$routeProvider.when('/categoryoverview/', {
+	  templateUrl: 'templates/boat/categoryoverview.html',
+	  controller: 'BoatCtrl'
+	});
+	$routeProvider.when('/boat/categoryoverview/', {
+	  templateUrl: 'templates/boat/categoryoverview.html',
+	  controller: 'BoatCtrl'
+	});
+	$routeProvider.when('/statoverview/', {
+	  templateUrl: 'templates/stats/statoverview.html',
+	  controller: 'StatCtrl'
+	});
+	$routeProvider.when('/', {redirectTo: '/boat/categoryoverview'});
+	$routeProvider.otherwise({
+	  templateUrl: 'templates/notimplementet.html',
+	});
+      }])
+    .config(['uiSelectConfig', function(uiSelectConfig) {
+      uiSelectConfig.theme = 'bootstrap';
+    }]);
