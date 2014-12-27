49db7f9baae2b132498e08b8dda85691d27794a9
Niels Elgaard Larsen <elgaard@agol.dk>
2014-12-27 01:42:35 +0100

    trip types with rights to json

--- backend/triptypes.php
+++ backend/triptypes.php
@@ -5,13 +5,11 @@ if ($rodb->connect_errno) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
- 
-if (!$rodb->set_charset("utf8")) {
-    printf("Error loading character set utf8: %s\n", $rodb->error);
-}
-
-    $s="SELECT TurTypeID as id, Navn as name
-    FROM TurType ORDER BY id";
+$s=<<<SQT
+select Navn,Beskrivelse, GROUP_CONCAT(required_right,':',requirement) as rights from TurType, TripRights WHERE aktiv AND trip_type=Navn GROUP BY TurType.Navn;
+SQT
+;
+// $s="SELECT TurTypeID as id, Navn as name FROM TurType ORDER BY id";
 
 
 // echo $s;
@@ -19,8 +17,15 @@ $result=$rodb->query($s) or die("Error in stat query: " . mysqli_error($rodb));;
 echo '[';
  $first=1;
  while ($row = $result->fetch_assoc()) {
-	  if ($first) $first=0; else echo ',';	  
-	  echo json_encode($row);
+	  if ($first) $first=0; else echo ',';
+      $rights=array();
+      $rg=explode(',', $row['rights']);
+      foreach ($rg as $ri) {
+          $ris=explode(":",$ri);
+          $rights[$ris[0]]=$ris[1];
+      }
+      $row['rights']=$rights;
+	  echo json_encode($row,JSON_PRETTY_PRINT);
 }
 echo ']';
 $rodb->close();
