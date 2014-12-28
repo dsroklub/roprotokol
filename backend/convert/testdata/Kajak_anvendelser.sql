97b30cd6e5841e2d9a04d2ff72e237cd833c4280
Niels Elgaard Larsen <elgaard@agol.dk>
2014-12-28 21:01:44 +0100

    testdata this is DSR data not containing personal data

---
+++ backend/convert/testdata/Kajak_anvendelser.sql
@@ -0,0 +1,7 @@
+INSERT INTO `Kajak_anvendelser` (`ID`, `Anvendelse`, `Beskrivelse`) VALUES (1,"Høje personer","Egnet til høje personer, god stabilitet");
+INSERT INTO `Kajak_anvendelser` (`ID`, `Anvendelse`, `Beskrivelse`) VALUES (2,"Kapkajak","Velegnet til kaproning");
+INSERT INTO `Kajak_anvendelser` (`ID`, `Anvendelse`, `Beskrivelse`) VALUES (3,"Instruktion","Anvendes til instruktion");
+INSERT INTO `Kajak_anvendelser` (`ID`, `Anvendelse`, `Beskrivelse`) VALUES (4,"Trækajak","n/a");
+INSERT INTO `Kajak_anvendelser` (`ID`, `Anvendelse`, `Beskrivelse`) VALUES (5,"Forbh. handicap tirsdag 17-20","Forbeholdt handicapløb tirsdag 17-20");
+INSERT INTO `Kajak_anvendelser` (`ID`, `Anvendelse`, `Beskrivelse`) VALUES (6,"Semikapkajak","n/a");
+INSERT INTO `Kajak_anvendelser` (`ID`, `Anvendelse`, `Beskrivelse`) VALUES (7,"Letvægtskapkajak","n/a");
