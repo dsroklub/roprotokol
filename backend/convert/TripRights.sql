5eae1340ff1974091610f485391fdebd9b228b60
Niels Elgaard Larsen <elgaard@agol.dk>
2014-12-28 21:00:45 +0100

    TripRights for DSR, this is the original for now

---
+++ backend/convert/TripRights.sql
@@ -0,0 +1,18 @@
+INSERT INTO TripRights (trip_type,required_right,requirement) VALUES ("Lokaltur","rowright","all");
+INSERT INTO TripRights (trip_type,required_right,requirement) VALUES ("Lokaltur","styrmand","any"); 
+
+INSERT INTO TripRights (trip_type,required_right,requirement) VALUES ("Langtur","langtursstyrmand","any");
+INSERT INTO TripRights (trip_type,required_right,requirement) VALUES ("Langtur","rowright","all"); 
+
+INSERT INTO TripRights (trip_type,required_right,requirement) VALUES ("Instruktion","instruktør","any");
+
+INSERT INTO TripRights (trip_type,required_right,requirement) VALUES ("Sved på Panden","rowright","all");
+INSERT INTO TripRights (trip_type,required_right,requirement) VALUES ("Sved på Panden","styrmand","any"); 
+
+INSERT INTO TripRights (trip_type,required_right,requirement) VALUES ("Puls og program","rowright","all");
+INSERT INTO TripRights (trip_type,required_right,requirement) VALUES ("Puls og program","styrmand","any"); 
+
+INSERT INTO TripRights (trip_type,required_right,requirement) VALUES ("Teknik+","styrmand","any");
+INSERT INTO TripRights (trip_type,required_right,requirement) VALUES ("Teknik+","rowright","any"); 
+
+INSERT INTO TripRights (trip_type,required_right,requirement) VALUES ("Outrigger/ScullerInstruktion","outriggerinstruktør","any"); 
