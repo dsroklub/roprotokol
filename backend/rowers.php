d0ca6a9c5e97151bbf886943713ef866055a0eb9
Niels Elgaard Larsen <elgaard@agol.dk>
2014-12-29 18:31:29 +0100

    add rights to rowers reorganise function utils.php update Readme on installation

--- backend/rowers.php
+++ backend/rowers.php
@@ -1,16 +1,18 @@
 <?php
 include("inc/common.php");
+include("inc/utils.php");
 
-$s="SELECT Medlemsnr as id,CONCAT(Fornavn,' ',Efternavn) as name,Initialer as initials
-    FROM Medlem";
+$s="SELECT Medlemsnr as id,CONCAT(Fornavn,' ',Efternavn) as name,Initialer as initials, GROUP_CONCAT(MemberRight,':',argument) as rights".
+    "  FROM Medlem,MemberRights Where MemberRights.MemberID=Medlem.MedlemID GROUP BY MemberID";
 
 
-// echo $s;
+echo $s."<br>";
 $result=$rodb->query($s) or die("Error in stat query: " . mysqli_error($rodb));;
 echo '[';
  $first=1;
  while ($row = $result->fetch_assoc()) {
 	  if ($first) $first=0; else echo ',';	  
+      $row['rights']=multifield($row['rights']);
 	  echo json_encode($row);
 }
 echo ']';
--- backend/triptypes.php
+++ backend/triptypes.php
@@ -1,5 +1,6 @@
 <?php
 include("inc/common.php");
+include("inc/utils.php");
 
 $s=<<<SQT
 select Navn,Beskrivelse, GROUP_CONCAT(required_right,':',requirement) as rights from TurType, TripRights WHERE aktiv AND trip_type=Navn GROUP BY TurType.Navn;
@@ -8,19 +9,12 @@ SQT
 // $s="SELECT TurTypeID as id, Navn as name FROM TurType ORDER BY id";
 
 
-// echo $s;
 $result=$rodb->query($s) or die("Error in stat query: " . mysqli_error($rodb));;
 echo '[';
  $first=1;
  while ($row = $result->fetch_assoc()) {
 	  if ($first) $first=0; else echo ',';
-      $rights=array();
-      $rg=explode(',', $row['rights']);
-      foreach ($rg as $ri) {
-          $ris=explode(":",$ri);
-          $rights[$ris[0]]=$ris[1];
-      }
-      $row['rights']=$rights;
+      $row['rights']=multifield($row['rights']);
 	  echo json_encode($row,JSON_PRETTY_PRINT);
 }
 echo ']';
