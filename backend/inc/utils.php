197da2158e992ae48da8c9c2da60a4d972067ffb
Niels Elgaard Larsen <elgaard@agol.dk>
2015-01-03 19:52:47 +0100

    newline

--- backend/inc/utils.php
+++ backend/inc/utils.php
@@ -1,15 +1,15 @@
 <?php
 function multifield($fld) {
-    $res=array();
-    $rg=explode(',', $fld);
-    foreach ($rg as $ri) {
-        $ris=explode(":",$ri);
-	if (count($ris) > 1) {
-	   $res[$ris[0]]=$ris[1];
-        } else {
-  	   error_log("Unparseable multifield: $fld");
-        }
+  $res=array();
+  $rg=explode(',', $fld);
+  foreach ($rg as $ri) {
+    $ris=explode(":",$ri);
+    if (count($ris) > 1) {
+      $res[$ris[0]]=$ris[1];
+    } else {
+      error_log("Unparseable multifield: $fld");
     }
-    return $res;
+  }
+  return $res;
 }
 ?>
