b63bdfb58313ac0fabfe931a56de361610b1b321
Niels Elgaard Larsen <elgaard@agol.dk>
2015-01-21 19:53:09 -0500

    more index and php listing updates

--- backend/index.php
+++ backend/index.php
@@ -2,5 +2,5 @@
 echo "<h2>Index of backend:</h2>";
 $g = glob("*.php");
 echo implode("<br>",
-	     array_map(function($a) {return '<a href="backend/'.$a.'">'.$a.'</a>';},$g)
+	     array_map(function($a) {return '<a href="'.$a.'">'.$a.'</a>';},$g)
 	     );
