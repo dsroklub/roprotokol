817c39e21f47ce660c8ad5bcce27a4e5caee28ec
Niels Elgaard Larsen <elgaard@agol.dk>
2015-02-12 21:11:50 +0100

    destinations

--- backend/boatdamages.php
+++ backend/boatdamages.php
@@ -5,7 +5,7 @@ header('Content-type: application/json');
 $s="SELECT Damage.id,Damage.Boat as boat_id,Boat.Name as boat,Damage.Description as description,Degree as level,ResponsibleMember,RepairerMember,Repaired as repaired, Damage.Created as created ".
     " FROM Damage,Boat WHERE Damage.Boat=Boat.id AND Repaired IS NULL ORDER BY Boat, level";
 
-// echo $s;
+//echo $s;
 $result=$rodb->query($s) or die("Error in stat query: " . mysqli_error($rodb));
 echo '[';
  $first=1;
