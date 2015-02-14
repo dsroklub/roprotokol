07141f61742eb39bc51b4d7dde0a5cbd0d40d169
Niels Elgaard Larsen <elgaard@agol.dk>
2015-02-15 00:54:44 +0100

    conistency check

--- backend/createtrip.php
+++ backend/createtrip.php
@@ -15,20 +15,35 @@ if (!$rodb->set_charset("utf8")) {
 
 $data = file_get_contents("php://input");
 $newtrip=json_decode($data);
+
+$rodb->query("BEGIN TRANSACTION");
+
+if ($stmt = $rodb->prepare("SELECT 'x' FROM  Trip WHERE BoatID=? AND InTime IS NULL")) { 
+  $stmt->bind_param('i', $newtrip->boat->id);
+  $stmt->execute();
+  $result= $stmt->get_result();
+  if ($result->fetch_assoc()) {
+    echo '{"error":"already on water"}';
+    $rodb->close();
+    exit(0);
+  }
+} 
+
 if ($stmt = $rodb->prepare("INSERT INTO Trip(Season,BoatID,Destination,TripTypeID,CreatedDate,EditDate,OutTime,ExpectedIn) VALUES(?,?,?,?,NOW(),NOW(),?,?)")) { 
      $stmt->bind_param('iisiss', $season, $newtrip->boat->id , $newtrip->destination->name, $newtrip->triptype->id, $newtrip->starttime, $newtrip->expectedtime);
      error_log('now EXE');
      $stmt->execute(); 
 } 
 
- if ($stmt = $rodb->prepare("INSERT INTO TripMember(TripID,Season,Seat,MemberID,MemberName,CreatedDate,EditDate)  VALUES(LAST_INSERT_ID(),?,?,?,?,NOW(),NOW())")) {
-     $seat=1;
-     foreach ($newtrip->rowers as $rower) {
-         $stmt->bind_param('iiis',$season,$seat, $rower->id,$rower->name);
-         $stmt->execute();
+if ($stmt = $rodb->prepare("INSERT INTO TripMember(TripID,Season,Seat,MemberID,MemberName,CreatedDate,EditDate)  VALUES(LAST_INSERT_ID(),?,?,?,?,NOW(),NOW())")) {
+  $seat=1;
+  foreach ($newtrip->rowers as $rower) {
+    $stmt->bind_param('iiis',$season,$seat, $rower->id,$rower->name);
+    $stmt->execute();
          $seat+=1;
-     }
- } 
+  }
+} 
+$rodb->query("END TRANSACTION");
 
 $rodb->close();
 ?> 
--- backend/onwater.php
+++ backend/onwater.php
@@ -3,17 +3,17 @@ include("inc/common.php");
 include("inc/utils.php");
 header('Content-type: application/json');
 
-$s="SELECT Boat.Name AS boat, OutTime as outtime, ExpectedIn as exptectedintime, Trip.Destination as destination, Trip.TripID as id, TripType.Name AS triptype ".
+$s="SELECT Boat.id as boatid, Boat.Name AS boat, OutTime as outtime, ExpectedIn as exptectedintime, Trip.Destination as destination, Trip.TripID as id, TripType.Name AS triptype ".
   " FROM TripType RIGHT JOIN (Boat RIGHT JOIN Trip ON Boat.id = Trip.BoatID) ON TripType.id = Trip.TripTypeID ".
   " WHERE ((Trip.InTime Is Null)) ORDER BY ExpectedIn";
 
-// echo $s;
+#echo $s;
 $result=$rodb->query($s) or die("Error in stat query: " . mysqli_error($rodb));;
 echo '[';
  $first=1;
  while ($row = $result->fetch_assoc()) {
-	  if ($first) $first=0; else echo ',';	  
-	  echo json_encode($row);
+	  if ($first) $first=0; else echo ',';
+	  echo json_encode($row,JSON_PRETTY_PRINT);
 }
 echo ']';
 $rodb->close();
