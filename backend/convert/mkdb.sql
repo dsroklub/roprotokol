4aedd491690751f667819e0cd892e4688489d669
Niels Elgaard Larsen <elgaard@agol.dk>
2016-01-14 17:32:01 +0100

    mulige medlemsrettigheder

--- backend/convert/mkdb.sql
+++ backend/convert/mkdb.sql
@@ -460,10 +460,22 @@ CREATE TABLE IF NOT EXISTS MemberRights (
        PRIMARY KEY(MemberID, MemberRight,Acquired,Argument)
 );
 
+CREATE TABLE IF NOT EXISTS MemberRightType (
+	member_right  VARCHAR(50) PRIMARY KEY,
+        description  VARCHAR(200)
+);
+
 
 CREATE TABLE IF NOT EXISTS TripRights (
        trip_type VARCHAR(30) NOT NULL,
        required_right VARCHAR(30) NOT NULL,
        requirement VARCHAR(10),
        PRIMARY KEY (trip_type,required_right)
+       );
+
+CREATE TABLE IF NOT EXISTS BoatRights (
+       boat_category VARCHAR(30) NOT NULL,
+       required_right VARCHAR(30) NOT NULL,
+       requirement VARCHAR(10),
+       PRIMARY KEY (boat_category,required_right)
        );
