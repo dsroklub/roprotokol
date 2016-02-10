e07dc34b7a8a827f74c741ad11e895900f8fbe0a
Niels Elgaard Larsen in DSR <elgaard@agol.dk>
2016-02-10 20:06:28 +0100

    type 1->0

--- rowingapp/frontend/app/components/utilities/utilities.js
+++ rowingapp/frontend/app/components/utilities/utilities.js
@@ -106,7 +106,7 @@ angular.module('myApp.utilities.subjecttodk', []).filter('subjecttodk', function
 });
 
 var damage_degrees={
-  1:'',
+  0: 'ok',
   1:'Let skadet',
   2: 'Middel skadet',
   3: 'Svært skadet'
