f18f99f3058ed1587fe4b46fdac11916c21b7dac
Niels Elgaard Larsen <elgaard@agol.dk>
2016-03-05 15:40:53 +0100

    exterm admin

--- externaladmin/import_members.php
+++ externaladmin/import_members.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <body>
@@ -6,7 +5,7 @@
 <form action="memberupload.php" method="post" enctype="multipart/form-data">
       Vælg medlemsdatabase (mdb eller accdb):
     <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload db" name="submit">
+    <input type="submit" value="Indsend medlemsdatabasen" name="submit">
 </form>
 
 </body>
