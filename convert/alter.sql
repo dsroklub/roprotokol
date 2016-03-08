4afadb651a1bd664f8ede8a34e4152074b4e52e3
Niels Elgaard Larsen in DSR <elgaard@agol.dk>
2016-03-08 21:25:43 +0100

    fix more bad DSR data

--- convert/alter.sql
+++ convert/alter.sql
@@ -8,3 +8,8 @@ Alter table Error_Trip add column EditDate DATE;
 ALTER TABLE BoatType CHANGE Name Name varchar(100) NOT NULL UNIQUE;
 ALTER TABLE Member CHANGE MemberID MemberID VARCHAR(10) NOT NULL;
 ALTER TABLE Member ADD COLUMN log VARCHAR(2000);
+
+UPDATE Trip SET Trip.OutTime=Trip.InTime WHERE Trip.OutTime IS NULL;
+UPDATE Trip SET Trip.OutTime="2025-05-03 12:00:00" WHERE Trip.OutTime="1025-05-03 12:00:00";
+
+select * from Trip WHERE Trip.OutTime < '2000';
