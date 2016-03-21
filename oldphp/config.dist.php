8b58ea768ab3654af887c8c59b3596424a45ecf1
Niels Elgaard Larsen <elgaard@agol.dk>
2016-03-21 19:08:27 +0100

    rm old PHP code that were never used

--- oldphp/config.dist.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config['database']['name'] = "roprotokol";
-$config['database']['username'] = "roprotokol";
-$config['database']['password'] = "roprotokol";
-$config['database']['hostname'] = "localhost";
-
