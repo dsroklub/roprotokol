d39c41d6838275a225b44ba49bc502df7c9704a0
Niels Elgaard Larsen <elgaard@agol.dk>
2016-04-19 21:49:55 -0400

    sql fixes

--- rowingapp/backend/errortrips.php
+++ rowingapp/backend/errortrips.php
@@ -4,20 +4,22 @@ include("inc/utils.php");
 header('Content-type: application/json');
   
 $sql=
-    " (SELECT ReasonForCorrection as reason,Error_Trip.id as id, Error_Trip.Trip, Error_Trip.Reporter, DeleteTrip,Boat.Name AS boat, TripType.Name as triptype, Error_Trip.Destination as destination, Error_Trip.CreatedDate as created, Distance as distance, TimeIn as intime, TimeOut as outtime,GROUP_CONCAT(CONCAT(Member.FirstName,' ',Member.LastName),':§§:',Member.MemberID ORDER BY Seat SEPARATOR '££' ) as rowers " .
-    " FROM Error_Trip LEFT JOIN Error_TripMember on Error_Trip.id=Error_TripMember.ErrorTripID LEFT JOIN Member on member_id=Member.id LEFT JOIN Boat on Boat.id=BoatID, TripType ".
-    " WHERE Fixed=0 AND (TripType.id=Error_Trip.TripTypeID OR Error_Trip.TripTypeID IS NULL) GROUP BY Error_Trip.id".
-    ")\n UNION " .
-    " (SELECT '' as reason, NULL as id, Trip.id as Trip, NULL as Reporter, NULL as DeleteTrip,Boat.Name AS boat, TripType.Name as triptype, Trip.Destination as destination, Trip.CreatedDate as created, Meter as distance, InTime as intime, OutTime as outtime,GROUP_CONCAT(CONCAT(Member.FirstName,' ',Member.LastName),':§§:',Member.MemberID ORDER BY Seat SEPARATOR '££') as rowers " .
-    " FROM Boat, Trip LEFT JOIN TripMember on Trip.id=TripMember.TripID LEFT JOIN Member on member_id=Member.id,Error_Trip,  TripType ".
-    " WHERE Boat.id=Trip.BoatID AND  Error_Trip.Trip=Trip.id AND TripType.id=Trip.TripTypeID GROUP BY Trip.id ".
-    ") ORDER BY Trip,id" ;
+    " (SELECT ReasonForCorrection as reason,Error_Trip.id as id, Error_Trip.Trip, Error_Trip.Reporter, DeleteTrip,Boat.Name AS boat, TripType.Name as triptype, Error_Trip.Destination as destination, Error_Trip.CreatedDate as created, Distance as distance, TimeIn as intime, TimeOut as outtime,GROUP_CONCAT(CONCAT(Member.FirstName,' ',Member.LastName),':§§:',Member.MemberID ORDER BY Seat SEPARATOR '££' ) as rowers
+    FROM Error_Trip LEFT JOIN Error_TripMember on Error_Trip.id=Error_TripMember.ErrorTripID LEFT JOIN Member on member_id=Member.id LEFT JOIN Boat on Boat.id=BoatID, TripType 
+    WHERE Fixed=0 AND (TripType.id=Error_Trip.TripTypeID OR Error_Trip.TripTypeID IS NULL) 
+    GROUP BY Error_Trip.id, TripType.Name) 
+  UNION 
+    (SELECT '' as reason, NULL as id, Trip.id as Trip, NULL as Reporter, NULL as DeleteTrip,Boat.Name AS boat, TripType.Name as triptype, Trip.Destination as destination, Trip.CreatedDate as created, Meter as distance, InTime as intime, OutTime as outtime,GROUP_CONCAT(CONCAT(Member.FirstName,' ',Member.LastName),':§§:',Member.MemberID ORDER BY Seat SEPARATOR '££') as rowers 
+     FROM Boat, Trip LEFT JOIN TripMember on Trip.id=TripMember.TripID LEFT JOIN Member on member_id=Member.id,Error_Trip,  TripType 
+     WHERE Boat.id=Trip.BoatID AND  Error_Trip.Trip=Trip.id AND TripType.id=Trip.TripTypeID GROUP BY Trip.id,TripType.Name
+    ) ORDER BY Trip,id" ;
 
 
  
 if ($sqldebug) echo $sql."\n\n";
 
 if ($stmt = $rodb->prepare($sql)) {
+    error_log("now exe");
      $stmt->execute() or die("Error in etrips query: " . mysqli_error($rodb));
      $result= $stmt->get_result() or die("Error in etrips query: " . mysqli_error($rodb));
 } else {
