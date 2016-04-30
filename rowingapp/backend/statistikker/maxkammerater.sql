fb0164a28d307449d8f218c72191a0f645d35986
Niels Elgaard Larsen <elgaard@agol.dk>
2016-04-30 03:43:18 +0200

    rowing buddies

--- rowingapp/backend/statistikker/maxkammerater.sql
+++ rowingapp/backend/statistikker/maxkammerater.sql
@@ -11,3 +11,10 @@ WHERE tm.TripID=Trip.id AND tm.member_id=Member.id AND tmo.TripID=Trip.id AND Me
 GROUP By Member.id
 ORDER BY 2015nummates DESC 
 LIMIT 10;
+
+SELECT Member.MemberID,Concat(Member.FirstName," ",Member.LastName) as name,COUNT(distinct tmo.member_id) as 2015nummates
+FROM Member, Trip,TripMember tm, TripMember tmo
+WHERE tm.TripID=Trip.id AND tm.member_id=Member.id AND tmo.TripID=Trip.id AND Member.id!=tmo.member_id AND YEAR(Trip.OutTime)="2015" AND Trip.TripTypeID!=5
+GROUP By Member.id
+ORDER BY 2015nummates DESC 
+LIMIT 10;
