f741be703d04846997659c63b77101fc536b5554
Niels Elgaard Larsen <elgaard@agol.dk>
2016-07-04 22:24:15 +0200

    external rights export as table instead of tuples

--- externaladmin/rights.php
+++ externaladmin/rights.php
@@ -3,13 +3,46 @@ set_include_path(get_include_path().':..');
 include("inc/common.php");
 header('Content-type: text/csv');
 header('Content-Disposition: filename="member_rights.csv"');
-$s='SELECT Concat(FirstName," ",LastName) as name, MemberID,MemberRight
+
+$cols=array();
+
+$sc="SELECT DISTINCT CONCAT(MemberRight,' ',argument) as mr from MemberRights ORDER BY mr";
+
+$result=$rodb->query($sc) or die("Error in column query: " . mysqli_error($rodb));;
+ while ($row = $result->fetch_assoc()) {
+     array_push($cols,$row["mr"]);
+ }
+
+$s='SELECT Concat(FirstName," ",LastName) as name, MemberID,CONCAT (MemberRight," ",argument) as mr, Acquired
     From Member,MemberRights 
     WHERE member_id=Member.id
-    ORDER BY MemberRight,name';
+    ORDER BY member_id,mr';
 
+if ($sqldebug) {
+ echo $s;
+ echo "\n\n";
+}
+
+$currentid=-1;
+$rower="";
+
+
+echo "Navn,medlemsnummer,".join(",",$cols)."\n";
 $result=$rodb->query($s) or die("Error in ld query: " . mysqli_error($rodb));;
  while ($row = $result->fetch_assoc()) {
-	  echo $row["name"].",".$row["MemberID"].",".$row["MemberRight"]."\n";
+     $mid=$row["MemberID"];
+     if ($mid != $currentid) {
+         if ($currentid>0) {
+             echo "\n".$rower.",".join(",",$rr);
+         }
+
+         foreach ($cols as $cl) {
+             $rr[$cl]="  ";
+         }         
+         $rower=$row["name"].",".$row["MemberID"];
+         $currentid=$mid;
+
+     }
+     $rr[$row["mr"]]=$row["Acquired"];
  }
 ?> 
