e52f536b700b91c9e55ee768ebfe7f9144f9d5ff
Niels Elgaard Larsen <elgaard@agol.dk>
2017-04-02 03:03:25 +0200

    mail by pw

---
+++ cox_instruction/inc/csvParse.php
@@ -0,0 +1,81 @@
+<?php 
+
+function csvParse( $input, $delimiter = ';', $textseperator = '"', $escape = false) {
+	$res = [];
+	$line = [];
+	$field = '';
+	
+	$length = strlen($input);
+	$cur;
+	$next;
+	$inQuote = false;
+	$prevDelim = false;
+	for ($i = 0; $i < $length; $i++) {
+		$cur = $input[$i];
+		$next = ($i == $length -1) ? null :$input[$i +1];
+		
+		if ( $cur == $textseperator ) {
+			if ($inQuote) {
+				if ($escape === false && $next == $textseperator) {
+					// Double quote is escaped quote
+					$field .= $cur;
+					$i++;
+				} else {
+					$inQuote = false;
+				}
+			} else {				
+				$inQuote = true;
+			}
+			$prevDelim = false;
+		} elseif ( $escape !== false && $cur == $escape ) {
+			$field .= $next;
+			$i++;
+			$prevDelim = false;
+		} elseif ( $cur == $delimiter ) {
+			if ($inQuote) {
+				$field .= $cur;
+				$prevDelim = false;
+			} else {
+				array_push($line, $field);
+				$field = '';
+				$prevDelim = true;
+			}
+		} elseif ( $cur == "\n" ) {
+			if ($inQuote) {
+				$field .= $cur;
+			} else {
+				if ($prevDelim || strlen($field) ) {
+					array_push($line, $field);
+				}
+				$field = '';
+				array_push($res, $line);
+				$line = [];
+			}
+			if ($next == "\r") {
+				$i++;
+			}
+			$prevDelim = false;
+		} else {
+			$field .= $cur;
+			$prevDelim = false;
+		}
+	}
+	
+	if ($inQuote) {
+		error_log("Parser error: Unbalanced quotes");
+		return false;
+	}
+	
+	if (strlen($field) || $prevDelim ) {
+		array_push($line, $field);
+	}
+	if (count($line)) {
+		array_push($res, $line);
+	}
+	
+	
+	
+	return $res;
+}
+
+?>
---
+++ cox_instruction/inc/db.php
@@ -0,0 +1,51 @@
+<?php
+$config = parse_ini_file($_SERVER['DOCUMENT_ROOT'].'/../config.ini');
+
+// include af denne fil medfører automatisk connection. Brug $link
+
+function print_array(&$a) {
+  echo "<hr><pre>";
+  print_r($a);
+  echo "</pre><hr>\n";
+}
+
+function sql_connect() {
+  global $config;
+  $link = new mysqli("localhost", $config["dbuser"], $config["dbpassword"], $config["database"]);
+
+  if ($link->connect_errno) {
+    printf("Could not connect to database: %s\n", $mysqli->connect_error);
+    exit();
+  }
+  $link->set_charset('utf8');
+  return $link;
+}
+
+function generate_password($len = 10) {
+   $vokaler = array('a', 'e', 'i', 'o', 'u', 'y');
+   $konsonanter = array('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z');
+   $res = '';
+   for ($i = 0; $i < $len; $i++) {
+	if ($i % 2 == 0) {
+	   $res .= $konsonanter[ mt_rand(0, 18) ];
+        } else {
+           $res .= $vokaler[ mt_rand(0, 5) ];
+	}
+   }
+
+   return $res;
+}
+
+function get_setting($name, $link = false) {
+  if (!$link) {
+    $link = $GLOBALS['link'];
+  }
+  $res = $link->query("SELECT * FROM settings WHERE name = '" . $link->escape_string($name) . "'");
+  if ($res && $row = $res->fetch_assoc() ) {
+    return $row['content'];
+  }
+  return "";
+}
+
+$link=sql_connect();
+?>
