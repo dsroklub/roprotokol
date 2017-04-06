8d639fa6bed16d27a997de696ccf529509704349
Niels Elgaard Larsen <elgaard@agol.dk>
2017-04-06 04:34:41 +0200

    mail

---
+++ public/inc/db.php
@@ -0,0 +1,39 @@
+<?php
+$config = parse_ini_file($_SERVER['DOCUMENT_ROOT'].'/../config.ini');
+
+// include af denne fil medfører automatisk connection. Brug $link
+
+function print_array(&$a) {
+  echo "<hr><pre>";
+  print_r($a);
+  echo "</pre><hr>\n";
+}
+
+function sql_connect() {
+  global $config;
+  $link = new mysqli("localhost", $config["loginuser"], $config["loginpw"], $config["database"]);
+
+  if ($link->connect_errno) {
+    printf("Could not connect to database: %s\n", $mysqli->connect_error);
+    exit();
+  }
+  $link->set_charset('utf8');
+  return $link;
+}
+
+function generate_password($len = 10) {
+   $vokaler = array('a', 'e', 'i', 'o', 'u', 'y');
+   $konsonanter = array('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z');
+   $res = '';
+   for ($i = 0; $i < $len; $i++) {
+	if ($i % 2 == 0) {
+	   $res .= $konsonanter[ mt_rand(0, 18) ];
+        } else {
+           $res .= $vokaler[ mt_rand(0, 5) ];
+	}
+   }
+   return $res;
+}
+
+$link=sql_connect();
+?>
---
+++ public/inc/mail_sender.php
@@ -0,0 +1,36 @@
+<?php
+  require_once("Mail.php");
+function send_email( $subject, $body, $user) {
+      $smtp = Mail::factory('sendmail',
+          array ());
+
+      $res = false;
+      if (isset($user['Email']) && trim($user['Email'])) {
+          $email = trim($user['Email']);
+
+          $mail_headers = array(
+                              'From'                      => "DSR Instruktionsrochef <instruktion@danskestudentersroklub.dk>",
+          					  'Reply-To'                  => "Niels Elgaard Larsen <elgaard@agol.dk>",
+                              'Content-Transfer-Encoding' => "8bit",
+						      'Content-Type'              => 'text/plain; charset="utf8"',
+						      'Date'                      => date('r'),
+			      			  'Message-ID'                => "<".sha1(microtime(true))."@instruktion.danskestudentersroklub.dk>",
+                              'MIME-Version'              => "1.0",
+                              'X-Mailer'                  => "PHP-Custom",
+                              'Subject'                   => "$subject"
+                               );
+
+          $mail_headers['To'] = $email;
+
+          $mail_content = $body;
+
+          $mail_status = $smtp->send($email, $mail_headers, $mail_content);
+          if (PEAR::isError($mail_status)) {
+   	      $res = "Kunne ikke sende mail til $email: " . $mail_status->getMessage();
+          }
+     } else {
+        $res = "Medlemsnummer " . $user['MemberId'] . " har ingen email-adresse!";
+     }
+     return $res;
+  }
+?>
