9a0b642a094d3d19b6c93b16b4f662408c60fb6a
Niels Elgaard Larsen <elgaard@agol.dk>
2017-04-12 02:41:08 +0200

    show teams with instructor names

--- cox/backend/aspirants/team.php
+++ cox/backend/aspirants/team.php
@@ -1,8 +1,9 @@
 <?php
 
 include("../../../rowing/backend/inc/common.php");
-$s='SELECT instruction_team.name,description,instructor
-    FROM instruction_team
+$s='SELECT instruction_team.name,description, Member.MemberId as instructor, CONCAT(Member.FirstName," ", Member.LastName) as instructor_name
+    FROM instruction_team, Member 
+    WHERE Member.id=instruction_team.instructor
     ORDER BY name
 ';
 $result=$rodb->query($s) or die("Error in stat query: " . mysqli_error($rodb));
