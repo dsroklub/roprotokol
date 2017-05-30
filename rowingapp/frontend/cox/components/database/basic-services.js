7fbd10b0334e0d0809e86904a1d464b0df38b170
Niels Elgaard Larsen <elgaard@agol.dk>
2017-05-30 23:59:24 +0200

    change pw

--- rowingapp/frontend/cox/components/database/basic-services.js
+++ rowingapp/frontend/cox/components/database/basic-services.js
@@ -9,6 +9,14 @@ this.getpw = function(data) {
     });
 }
 
+this.setpw = function(data) {
+    $http.post('../../../backend/event/setpw.php', data).then(function(r) {
+      alert("password skiftet");
+    },function(r) {
+      alert("det mislykkedes at skifte password");
+    });
+}
+
 
   this.logout = function() {
     $http.get('../../../backend/cox/aspirants/logout.php').then(function(r) {
