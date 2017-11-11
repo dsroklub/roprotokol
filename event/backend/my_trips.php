9fc51bcbc4e4abc887423f14ac981c6ad0955be1
Niels Elgaard Larsen <elgaard@agol.dk>
2017-11-11 19:04:56 +0100

    gym and trips

--- event/backend/my_trips.php
+++ event/backend/my_trips.php
@@ -12,11 +12,15 @@ $s="SELECT OutTime as tid,Boat.name as boat,Trip.Meter/1000 as km ,Trip.Destinat
     FROM Trip,TripMember,Member,Boat
     WHERE TripMember.TripID=Trip.id AND Member.id=TripMember.member_id AND Member.MemberID=? 
        AND Boat.id=Trip.BoatId
-    ORDER BY OutTime DESC";
+UNION
+   SELECT start_time as tid, team as boat, 0 as km, '' as destination
+   FROM team_participation, Member
+   WHERE team_participation.member_id=Member.id AND Member.MemberId=?
+ORDER BY tid DESC";
 
-#echo $s;
+if ($sqldebug) echo $s;
 if ($stmt = $rodb->prepare($s)) {
-    $stmt->bind_param("s",$cuser);
+    $stmt->bind_param("ss",$cuser,$cuser);
      $stmt->execute(); 
      $result= $stmt->get_result();     
      while ($row = $result->fetch_assoc()) {
