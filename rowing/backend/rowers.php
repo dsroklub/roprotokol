3e64fe15db1b27b60eff059e812df510a57472bf
Niels Elgaard Larsen <elgaard@agol.dk>
2017-12-23 23:26:45 +0100

    first queries to use JSON_OBJECT

--- rowing/backend/rowers.php
+++ rowing/backend/rowers.php
@@ -3,10 +3,19 @@ include("inc/common.php");
 include("inc/utils.php");
 header('Content-type: application/json');
 
-$s="SELECT Member.MemberID AS id,CONCAT(FirstName,' ',LastName) AS name, GROUP_CONCAT(MemberRight,':§§:',argument, ':§§:',Acquired SEPARATOR '££') AS rights".
-    "  FROM Member LEFT JOIN MemberRights on MemberRights.member_id=Member.id  WHERE Member.MemberID!='0' GROUP BY Member.id";
 
-# Member.MemberID should not be necessary, non members should have MemberID=NULL, not 0
+
+// TODO when we can use Mysql 8 replace with JSON_ARRAYAGG etc
+$s="SELECT JSON_MERGE(
+    JSON_OBJECT(
+      'id',Member.MemberID, 
+      'name', CONCAT(FirstName,' ',LastName) 
+   ),
+   CONCAT('{\"rights\" : [',GROUP_CONCAT(JSON_OBJECT('member_right',MemberRight,'arg',argument,'acquired',Acquired)),']}')
+   ) AS json
+   FROM Member LEFT JOIN MemberRights on MemberRights.member_id=Member.id  
+   WHERE Member.MemberID!='0'
+   GROUP BY Member.id";
 
 if ($sqldebug) {
     echo $s."<br>\n";
@@ -16,8 +25,7 @@ echo '[';
  $first=1;
  while ($row = $result->fetch_assoc()) {
 	  if ($first) $first=0; else echo ',';	  
-      $row['rights']=multifield_array($row['rights'],["member_right","arg","acquired"]);
-	  echo json_encode($row); // ,JSON_PRETTY_PRINT
+	  echo $row['json'];
 }
 echo ']';
 $rodb->close();
