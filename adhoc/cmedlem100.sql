669a679beba894c8bf9ea9e50e747cafd62f82ac
Niels Elgaard Larsen <elgaard@agol.dk>
2019-03-26 14:54:08 +0100

    small fixes

--- adhoc/cmedlem100.sql
+++ adhoc/cmedlem100.sql
@@ -1,7 +1,9 @@
-SELECT "robåde", aar as "år",COUNT(id) plus100 FROM (
- SELECT YEAR(OutTime) as aar,id,SUM(Meter) as distance
-   FROM TripMember,Trip
+SELECT "robåde   ",aar as "år",COUNT(id) plus100 FROM (
+ SELECT YEAR(OutTime) as aar,Member.id,SUM(Meter) as distance
+   FROM TripMember,Trip,Member
    WHERE
+   Member.id=TripMember.member_id AND
+   Member.MemberId REGEXP '^[0-9]+' AND
    TripMember.TripID=Trip.id AND
    Trip.BoatID IN (SELECT id FROM Boat,BoatType WHERE boat_type=BoatType.name AND BoatType.category=2)
    GROUP BY TripMember.member_id,aar
@@ -10,9 +12,11 @@ SELECT "robåde", aar as "år",COUNT(id) plus100 FROM (
  GROUP by aar;
 
 SELECT "inrigger",aar as "år",COUNT(id) plus100 FROM (
- SELECT YEAR(OutTime) as aar,id,SUM(Meter) as distance
-   FROM TripMember,Trip
+ SELECT YEAR(OutTime) as aar,Member.id,SUM(Meter) as distance
+   FROM TripMember,Trip,Member
    WHERE
+   Member.id=TripMember.member_id AND
+   Member.MemberId REGEXP '^[0-9]+' AND
    TripMember.TripID=Trip.id AND
    Trip.BoatID IN (SELECT id FROM Boat WHERE boat_type LIKE "Inrigger%")
    GROUP BY TripMember.member_id,aar
