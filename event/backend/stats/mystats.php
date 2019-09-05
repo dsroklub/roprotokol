0ec8cb9863a82260ab3782886ac2227fd1683bd3
Niels Elgaard Larsen <elgaard@agol.dk>
2019-09-05 16:24:06 +0200

    more stat boxes

--- event/backend/stats/mystats.php
+++ event/backend/stats/mystats.php
@@ -7,40 +7,52 @@ if (isset($_GET["q"])) {
     $q=$_GET["q"];
 }
 
-if ($q=="mates") {
+switch ($q) {
+case "rights":
+    $s="SELECT showname as member_right,argument as arg,DATE_FORMAT(acquired,'%Y-%m-%dT%T') as acquired
+    FROM MemberRights, Member, right_name
+    WHERE Member.MembeRID=? AND Member.id=MemberRights.member_id AND right_name.member_right=MemberRight
+    ORDER BY acquired";
+    break;
+case "mates":
     $s="SELECT CONCAT(them.FirstName,' ',them.LastName) as mate, SUM(Meter) as dist 
     FROM Member me, Member them,Trip,TripMember tm, TripMember ttm 
     WHERE me.MemberID=? AND tm.TripID=Trip.id AND tm.member_id=me.id AND them.id=ttm.member_id and ttm.TripID=Trip.id AND me.id!=them.id 
     GROUP By mate 
     ORDER BY dist DESC 
     LIMIT 10";
-} else if ($q=="boats") {
+    break;
+case "boats":
     $s="SELECT Boat.Name as boatname, SUM(Meter) as dist 
     FROM Member me, Boat,Trip,TripMember tm
     WHERE me.MemberID=? AND tm.member_id=me.id AND Trip.id=tm.TripID AND Trip.BoatID=Boat.id
     GROUP By Boat.id 
     ORDER BY dist DESC 
     LIMIT 10";
-} else if ($q=="destinations") {
+    break;
+case "destinations":
     $s="SELECT Trip.Destination AS destination, COUNT(Trip.id) as numtrips
     FROM Member me,Trip,TripMember tm
     WHERE me.MemberID=? AND tm.member_id=me.id AND Trip.id=tm.TripID 
     GROUP By Trip.Destination 
     ORDER BY numtrips DESC 
     LIMIT 10";
-} else if ($q=="triptypes") {
+    break;
+case "triptypes":
     $s="SELECT TripType.Name AS triptype, COUNT(Trip.id) as numtrips
     FROM Member me,Trip,TripMember tm,TripType
     WHERE me.MemberID=? AND tm.member_id=me.id AND Trip.id=tm.TripID AND TripType.id=Trip.TripTypeID
     GROUP By TripType.id
     ORDER BY numtrips DESC 
     LIMIT 20";
-} else {
-    echo "invalid query ".$q;
+    break;
+default:    
+    $res=["status" => "error", "error"=>"invalid query' .$q"];
+    echo json_encode($res);
     exit(0);
 }
 
-if ($stmt = $rodb->prepare($s)) {
+$stmt = $rodb->prepare($s) or dbErr($rodb,$res,"mystats $q");
     $stmt->bind_param("s",$cuser);
      $stmt->execute(); 
      $result= $stmt->get_result();
@@ -53,5 +65,4 @@ if ($stmt = $rodb->prepare($s)) {
      }
      echo ']';     
      $stmt->close(); 
- } 
 $rodb->close();
