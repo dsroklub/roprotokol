448b834969061aa7b1bf60b13eb7a2ba301c77f5
Niels Elgaard Larsen <elgaard@agol.dk>
2019-10-25 01:18:29 +0200

    interval fixdate

--- rowingapp/frontend/dsrcommon/components/utilities/interval.js
+++ rowingapp/frontend/dsrcommon/components/utilities/interval.js
@@ -12,7 +12,6 @@ function iCtrl() {
       this.ngModel.end_time=this.et.time;
     }
     if (this.ngModel.end_time && this.ngModel.start_time && this.ngModel.end_time<this.ngModel.start_time) {
-      console.log("T1");
       this.ngModel.end_time.setDate(this.ngModel.end_time.getDate()+1);
     }
     if (this.ngModel.end_time>now) {
@@ -25,7 +24,7 @@ angular.module('dsrcommon.utilities.dsrinterval',[]).
   component('dsrinterval',{
     replace:true,
     template:
-    '<span ng-style="$ctrl.hmstyle"><dsrtime ng-model="$ctrl.st" on-update="$ctrl.fixdate($event)"></dsrtime>&mdash;<dsrtime ng-model="$ctrl.et" on-update="$ctrl.fixdate($event)"></dsrtime> </span>',
+    '<span ng-style="$ctrl.hmstyle"><dsrtime ng-model="$ctrl.st" fixdate="$ctrl.fixdate()"></dsrtime>&mdash;<dsrtime ng-model="$ctrl.et"  fixdate="$ctrl.fixdate()"></dsrtime> </span>',
     bindings: {
       ngModel: "=",
       onUpdate: '&'
