2179e4ea259572f135e56a2987e241683a86c3eb
Niels Elgaard Larsen <elgaard@agol.dk>
2019-11-02 22:12:46 +0100

    hours rounding

--- event/backend/update_work.php
+++ event/backend/update_work.php
@@ -1,6 +1,6 @@
 <?php
-require("inc/utils.php");
 include("../../rowing/backend/inc/common.php");
+require("utils.php");
 
 //verify_real_user("registrere timer");
 $data = file_get_contents("php://input");
@@ -8,6 +8,10 @@ $d=json_decode($data);
 
 function parse_time($t) {
     return($t->year."-".$t->month."-".$t->day." ".$t->hour.":".$t->minute);
+}
+
+if ($cuser != "baadhal" ) {
+    verify_right("admin","vedligehold");
 }
 $stmt=$rodb->prepare("UPDATE worklog 
     SET start_time=?,end_time=?, work=?, boat=?, hours=?, task=?
@@ -28,9 +32,10 @@ if (isset($d->hours)) {
     $hours=$d->hours;
 } else {
     $hours=(strtotime($end_time)-strtotime($start_time))/3600;
-    if ($hours>3){
-        $hours=3;
-    }
+}
+
+if ($cuser=="baadhal"  && $hours>3){
+    $hours=3;
 }
 $res["hours"]=$hours;
 error_log("XXX $d->work, $d->boat, $hours,$d->id");
