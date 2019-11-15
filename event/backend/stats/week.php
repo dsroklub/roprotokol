da085b073c38fe36ee160cae55c62b64a8886a97
Niels Elgaard Larsen <elgaard@agol.dk>
2019-11-15 01:26:35 +0100

    improve week graph

--- event/backend/stats/week.php
+++ event/backend/stats/week.php
@@ -4,15 +4,60 @@ include("../../../rowing/backend/inc/common.php");
 require_once("utils.php");
 
 $report_name="ugegraf";
-$s="SELECT WEEK(start_time) as uge, SUM(hours) as timer, GROUP_CONCAT(DISTINCT boat)  as både FROM worklog GROUP BY uge ORDER BY uge";
+
+$l = $rodb->query("SELECT SUM(requirement) AS 'timer' FROM worker WHERE assigner='vedligehold'")->fetch_assoc()["timer"] or dbErr($rodb,$res,"weeksum $q");
+
+$s="SELECT WEEK(start_time) as uge, SUM(hours) as timer, GROUP_CONCAT(DISTINCT boat)  as både FROM worklog GROUP BY uge ORDER BY start_time";
 
 $result = $rodb->query($s) or dbErr($rodb,$res,"week $q");
 $data=[];
 $weeks=[];
+$left=[];
+$gns=[];
+$numweeks=$leftweeks=14+52-41+1;
+$avg=round($l/$numweeks,1);
 while ($row = $result->fetch_assoc()) {
-    $weeks[$row["uge"]]=$row["timer"];
+    $weeks[$row["uge"]]=round($row["timer"],1);
+    $l=$l-$row["timer"];
+    $left[$row["uge"]]=round($l,1);
+    $gns[$row["uge"]]=$avg;
+    $leftweeks--;
+    $w=$row["uge"];
 }
-$wd=["labels"=>array_keys($weeks), series=>["alt"],data=>[array_values($weeks)]];
+$w++;
+$perweek=$l/$leftweeks;
+$weeks[$w]=0;
+$left[$w]=round(($leftweeks-1)*$perweek,1);
+$gns[$w]=round($perweek,1);
+$w++;
+while ($w > 42 || $w<13 ) {
+    $weeks[$w]=0;
+    $left[$w]=null;
+    $gns[$w]=null;
+    $w++;
+    if ($w>52) $w=1;
+}
+$left[$w]=0;
+$gns[$w]=round($perweek,1);
 
-echo json_encode($wd);
+$wd=[
+    "labels"=>array_keys($weeks),
+    "series"=>["gjort","tilbage","gns"],
+    "data"=>[array_values($weeks),array_values($left),array_values($gns)],
+    "override"=>[
+        ["yAxisID"=>"y1","type"=>"bar","label"=>"ugetimer"],
+        ["yAxisID"=>"y2","type"=>"line","label"=>"tilbage","spanGaps"=>true],
+        ["yAxisID"=>"y1","type"=>"line","label"=>"gennemsnit","spanGaps"=>true,"lineTension"=>0]
+    ],
+    "options"=>[
+        "scales"=>[
+            "yAxes"=>[
+                ["id"=>"y1","position"=>"left","type"=>"linear"],
+                ["id"=>"y2","position"=>"right","type"=>"linear","display"=>true]
+            ]
+        ]
+    ]
+];
+
+echo json_encode($wd,JSON_PRETTY_PRINT);
 $rodb->close();
