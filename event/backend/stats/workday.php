abc5a001b8d90f4498af11b12ffe2de26d569f42
Niels Elgaard Larsen <elgaard@agol.dk>
2020-01-05 19:43:34 +0100

    workday

---
+++ event/backend/stats/workday.php
@@ -0,0 +1,28 @@
+<?php
+
+include("../../../rowing/backend/inc/common.php");
+include("utils.php");
+
+$day=$_GET["day"];
+// assert($cuser=="baadhal" || $cuser=="7843");
+$s="SELECT JSON_OBJECT(
+    'id',worklog.id,
+    'start_time',JSON_OBJECT('year',YEAR(start_time),'month',MONTH(start_time),'day',DAY(start_time),'hour',HOUR(start_time),'minute',MINUTE(start_time)),
+    'end_time',JSON_OBJECT('year',YEAR(worklog.end_time),'month',MONTH(worklog.end_time),'day',DAY(worklog.end_time),'hour',HOUR(worklog.end_time),'minute',MINUTE(worklog.end_time)),
+    'hours',hours,
+    'task',task,
+    'boat',boat,
+    'created',worklog.created,
+    'work',work
+    ) as json
+    FROM worklog, Member
+    WHERE DATE(start_time)=DATE(?) AND Member.id=worklog.member_id
+    ORDER BY start_time";
+
+$stmt = $rodb->prepare($s) or dbErr($rodb,$res,"mystats $q");
+$stmt->bind_param("s",$day);
+$stmt->execute() ||  dbErr($rodb,$res,"workday $q");
+$result= $stmt->get_result();
+output_json($result);
+$stmt->close();
+$rodb->close();
