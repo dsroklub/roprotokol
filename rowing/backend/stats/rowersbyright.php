3fb720683ded50a1b224505b26cad6ddbb75a377
Niels Elgaard Larsen in DSR <elgaard@agol.dk>
2020-02-19 23:48:25 +0100

    handle args

--- rowing/backend/stats/rowersbyright.php
+++ rowing/backend/stats/rowersbyright.php
@@ -2,26 +2,33 @@
 set_include_path(get_include_path().':..');
 include("inc/common.php");
 
-assert(isset($_GET["right"]) && isset($_GET["subtype"]));
+assert(isset($_GET["right"]));
 $right=$_GET["right"];
-$subtype=$_GET["subtype"];
+$subtype=$_GET["subtype"]??"";
 
-$s="SELECT CONCAT(Member.FirstName,' ',Member.LastName) AS rower, Acquired as tildelt
+$s =empty($subtype)?
+   "SELECT CONCAT(Member.FirstName,' ',Member.LastName) AS rower, Acquired as tildelt, argument as subtype
     FROM Member, MemberRights
-    WHERE Member.id=MemberRights.member_id and MemberRight=? AND argument=?";
+    WHERE Member.id=MemberRights.member_id AND RemoveDate IS NULL AND MemberRight=? "
+   :
+   "SELECT CONCAT(Member.FirstName,' ',Member.LastName) AS rower, Acquired as tildelt
+    FROM Member, MemberRights
+    WHERE Member.id=MemberRights.member_id AND RemoveDate IS NULL AND MemberRight=? AND argument=?"
+   ;
+
 
 if ($sqldebug) {
     echo $s;
 }
 
-if ($stmt = $rodb->prepare($s)) {
-    error_log("RBR 4 r=$right, s=$subtype");
-    $stmt->bind_param("ss",$right,$subtype);
-     $stmt->execute();
-     $result= $stmt->get_result();
-     process($result,"csv","roere",array("roer","pr"));
-     $stmt->close();
+$stmt = $rodb->prepare($s) or dbErr($rodb,$res,"RBR");
+if (empty($subtype)) {
+    $stmt->bind_param("s",$right);
 } else {
-    error_log($rodb->error);
+    $stmt->bind_param("ss",$right,$subtype);
 }
+$stmt->execute() || dbErr($rodb,$res,"RBR exe");
+$result= $stmt->get_result();
+process($result,"csv","Ret_$right$subtype",array("$right","pr"));
+$stmt->close();
 $rodb->close();
