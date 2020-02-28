1ffc82ec7245d14a0180a2ae2d143472b6e9ad83
Niels Elgaard Larsen <elgaard@agol.dk>
2020-02-28 11:50:08 +0100

    instruktorstat

--- rowing/backend/stats/longdistancecoxes.php
+++ rowing/backend/stats/longdistancecoxes.php
@@ -5,11 +5,11 @@ include("inc/backheader.php");
 header('Content-type: text/csv');
 header('Content-Disposition: filename="langtursstyrmaend.csv"');
 $s='SELECT Concat(FirstName," ",LastName) as name, MemberID,MemberRight
-    From Member,MemberRights 
+    FROM Member,MemberRights
     WHERE member_id=Member.id AND (MemberRight="longdistance" OR MemberRight="longdistancetheory")
     ORDER BY MemberRight,name';
 
 $result=$rodb->query($s) or die("Error in ld query: " . mysqli_error($rodb));;
  while ($row = $result->fetch_assoc()) {
-	  echo $row["name"].",".$row["MemberID"].",".$row["MemberRight"]."\n";
+     echo $row["name"].",".$row["MemberID"].",".$row["MemberRight"]."\n";
  }
