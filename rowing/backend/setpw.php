98eb92d123bb48ac29e74fb771ba61b1026c9857
Niels Elgaard Larsen <elgaard@agol.dk>
2020-05-15 18:15:21 +0200

    rm html-bin, unsafe

--- rowing/backend/setpw.php
+++ rowing/backend/setpw.php
@@ -1,5 +1,5 @@
 <?php
-$r=array ("status" => "ok");
+$res=array ("status" => "ok");
 $json = file_get_contents("php://input");
 $data=json_decode($json);
 $cuser=$_SERVER['PHP_AUTH_USER'];
@@ -13,12 +13,7 @@ $memberId=$cuser;
 $hpw= '{SHA}' . base64_encode(sha1($pw, TRUE));
 error_log("new  pw= $pw $hpw mid=$memberId");
 
-if ($istmt = $rodb->prepare(
-    "UPDATE authentication SET password=?,newpassword=? WHERE member_id in (SELECT id from Member where MemberId=?)")) {
-                error_log("now Bind");
-                $istmt->bind_param('sss', $hpw,$pw,$memberId) || error_log($link->error);
-                error_log("now EXE");
-                $istmt->execute() || error_log("pw update error: ". $link->error);                            
-} else {
-    error_log("Prepare Error:". $link->error);
-}
+$istmt = $rodb->prepare("UPDATE authentication SET password=?,newpassword=? WHERE member_id in (SELECT id from Member where MemberId=?)") or dbErr($rodb,$res,"setpw");
+$istmt->bind_param('sss', $hpw,$pw,$memberId) || dbErr($rodb,$res,"pwerr");
+$istmt->execute() || dbErr($rodb,$res,"pwerr");
+echo json_encode($res);
