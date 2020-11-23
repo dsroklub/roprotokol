b6ea28104344270ba022fe965f61e4772fa0af47
Niels Elgaard Larsen <elgaard@agol.dk>
2020-11-23 12:03:26 +0100

    week number function

--- rowingapp/frontend/event/controllers/work.js
+++ rowingapp/frontend/event/controllers/work.js
@@ -15,6 +15,18 @@ function toDateTime(w) {
 }
 
 
+function week_no(dt) {
+  var tdt = new Date(dt.valueOf());
+  var dayn = (dt.getDay() + 6) % 7;
+  tdt.setDate(tdt.getDate() - dayn + 3);
+  var firstThursday = tdt.valueOf();
+  tdt.setMonth(0, 1);
+  if (tdt.getDay() !== 4) {
+    tdt.setMonth(0, 1 + ((4 - tdt.getDay()) + 7) % 7);
+  }
+  return 1 + Math.ceil((firstThursday - tdt) / 604800000);
+}
+
 function workCtrl ($scope, $routeParams,$route,DatabaseService, LoginService, $filter, ngDialog, orderBy, $log, $location,$anchorScroll,$timeout) {
   $scope.work={};
   $scope.workers=[];
