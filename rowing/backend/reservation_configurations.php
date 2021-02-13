f7916cb9d1b9ec1a28d047bc6149745c7b0f3003
Niels Elgaard Larsen <elgaard@agol.dk>
2021-02-13 16:32:48 +0100

    get res confs

---
+++ rowing/backend/reservation_configurations.php
@@ -0,0 +1,9 @@
+<?php
+include("inc/common.php");
+include("inc/utils.php");
+$s="SELECT name,selected FROM reservation_configuration ORDER by name";
+$stmt = $rodb->prepare($s) or dbErr($rodb,$res,"get rc");
+$stmt->execute();
+$result= $stmt->get_result() or dbErr($rodb,$res,"GET res conf");
+output_rows($result);
+$rodb->close();
