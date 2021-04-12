915b5bf9cdc1bca7560b0819cb36a48c2fe7bb4c
Niels Elgaard Larsen <elgaard@agol.dk>
2021-04-12 13:52:49 +0200

    swimtest report

---
+++ rowing/backend/stats/swimtest3year.php
@@ -0,0 +1,16 @@
+<?php
+set_include_path(get_include_path().':..');
+include("inc/common.php");
+include("inc/backheader.php");
+
+$s="SELECT CONCAT(FirstName,' ',LastName) as roer,MemberId as medlemsnummer, IFNULL(MAX(DATE(MemberRights.Acquired)),'') as svømmeprøve,
+IF(DATEDIFF(DATE(NOW()),MAX(DATE(MemberRights.Acquired)))<3*365,'OK','-') AS 'indenfor 3 år'
+FROM Member LEFT JOIN MemberRights on  MemberRights.member_id=Member.id AND MemberRights.MemberRight='entringsøvelse'
+WHERE
+  RemoveDate IS NULL
+Group By Member.id
+ORDER BY FirstName,LastName
+";
+$result=$rodb->query($s) or dbErr($rodb,$res,"Error in swim300 query: " );
+$output='xlsx';
+process($result,$output,"langturs svømmeprøver","_auto");
