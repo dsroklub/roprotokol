a4dd4c7e14844a1b514c0ce0b40b49d8f107d5ae
Niels Elgaard Larsen <elgaard@agol.dk>
2021-05-01 15:42:44 +0200

    use expexted intime

--- public/boat_status.php
+++ public/boat_status.php
@@ -12,7 +12,7 @@
 <th>B&aring;dtype</th>
 <th>placering</th>
 <th>skade</th>
-<th>udskrevet</th>
+<th>forventes ind</th>
       </tr>
       <?php
          ini_set('default_charset', 'utf-8');
@@ -27,9 +27,9 @@
 $boatclause=" ";
 
 $s="
-SELECT b.boat_name, b.boat_type, IFNULL(DamageType.name,'') as damage,out_time,location
+SELECT b.boat_name, b.boat_type, IFNULL(DamageType.name,'') as damage,out_time,location,expected
   FROM (
-  SELECT Boat.id,Boat.Name AS boat_name, Boat.Location as location, Boat.boat_type, COALESCE(MAX(Damage.Degree),0) as damage,Trip.OutTime as out_time
+  SELECT Boat.id,Boat.Name AS boat_name, Boat.Location as location, Boat.boat_type, COALESCE(MAX(Damage.Degree),0) as damage,Trip.OutTime as out_time, DATE_FORMAT(Trip.ExpectedIn,'%Y-%m-%d %H:%i') as expected
 FROM
  Boat
    LEFT OUTER JOIN Damage ON (Damage.Boat=Boat.id AND Damage.Repaired IS NULL) JOIN DamageType
@@ -48,7 +48,7 @@ ORDER BY location, boat_type,boat_name
 if ($stmt = $rodb->prepare($s)) {
       $result=$rodb->query($s) or die("Error in instruktion stat query: " . mysqli_error($rodb));;
       while ($row = $result->fetch_assoc()) {
-      print("<tr><td>".$row['boat_name']."</td><td>".$row['boat_type']."</td><td class='nr'>".$row['location']."</td><td>".$row['damage']."</td><td>".$row['out_time']."</td></tr>\n");
+      print("<tr><td>".$row['boat_name']."</td><td>".$row['boat_type']."</td><td class='nr'>".$row['location']."</td><td>".$row['damage']."</td><td>".$row['expected']."</td></tr>\n");
       }
       }  else {
       error_log("SQL boat stat error: ".$rodb->error);
