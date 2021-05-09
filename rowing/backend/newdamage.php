25e860fbe0ae45beee02cc385b230d0b0ef4271e
Niels Elgaard Larsen <elgaard@agol.dk>
2021-05-10 01:33:23 +0200

    errror handling for damage

--- rowing/backend/newdamage.php
+++ rowing/backend/newdamage.php
@@ -12,19 +12,9 @@ if (empty($cuser)) {
 } else {
     $reporter=$cuser;
 }
-    
-if ($stmt = $rodb->prepare("
-INSERT INTO Damage(Boat,Degree,ResponsibleMember,Description,Created) 
-SELECT ?,?,id,?,NOW() 
-From Member WHERE MemberID=?")) { 
-    $stmt->bind_param('iiss', $newdamage->boat->id , $newdamage->degree, $newdamage->description,$reporter);
-    if (!$stmt->execute()) {
-        error_log("could not create damage, DB error".$stmt->error);
-    } 
-} else {
-    error_log("new damage database error".$stmt->error);
-}
-
+$stmt = $rodb->prepare("INSERT INTO Damage(Boat,Degree,ResponsibleMember,Description,Created) SELECT ?,?,id,?,NOW() From Member WHERE MemberID=?")) or dbErr($rodb,$res,"newdamage prep");
+$stmt->bind_param('iiss', $newdamage->boat->id , $newdamage->degree, $newdamage->description,$reporter) || dbErr($rodb,$res,"newdamage bind");
+$stmt->execute() || dbErr($rodb,$res,"could not create damage, DB error".$stmt->error);
 eventLog($newdamage->reporter->name." meldte skaden: ".$newdamage->description. " grad ".$newdamage->degree." på båden ".$newdamage->boat->name);
 $result=$rodb->query("SELECT LAST_INSERT_ID() AS id FROM DUAL") or die ("Error in new id query: " . mysqli_error($rodb));
 $nid = $result->fetch_assoc();
@@ -33,11 +23,8 @@ $newdamage->id=$nid['id'];
 $newdamage->boat_id=$newdamage->boat->id;
 $newdamage->boat=$newdamage->boat->name;
 $newdamage->reporter=$newdamage->reporter->name;
-
 $res['damage']=$newdamage;
 $rodb->query("END TRANSACTION");
-
 invalidate('boat');
 $rodb->close();
 echo json_encode($res);
-
