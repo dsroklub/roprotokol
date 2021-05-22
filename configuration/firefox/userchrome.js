27aea1528acb0cff317665507983d0b6ef50105b
Niels Elgaard Larsen in DSR <elgaard@agol.dk>
2021-05-22 14:39:38 +0200

    client config

---
+++ configuration/firefox/userchrome.js
@@ -0,0 +1,4 @@
+# https://superuser.com/questions/821594/firefox-change-disable-default-keyboard-shortcut-configuration
+
+var key = document.getElementById('viewBookmarksSidebarKb');
+if (key) key.remove();
