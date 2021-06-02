ee8134dba101537a97017b87002d2815d59ddfcd
Niels Elgaard Larsen <elgaard@agol.dk>
2021-06-02 22:56:21 +0200

    year in stat name

--- rowing/backend/stats/newrights_year.php
+++ rowing/backend/stats/newrights_year.php
@@ -3,7 +3,7 @@ set_include_path(get_include_path().':..');
 include("inc/common.php");
 include("inc/backheader.php");
 $now = getdate();
-$year = isset($_GET["year"]) ? (int) $_GET["year"] : $now['year'];
+$year = (int) ($_GET["year"] ??  $now['year']);
 $s="SELECT Concat(Member.FirstName,' ',Member.LastName) as roer, Member.MemberID as medlemNr,showname as rettighed,argument as ekstra,Acquired as tildelt,Concat(m.FirstName,' ',m.LastName) as tildeler
     FROM Member,MemberRightType,MemberRights LEFT JOIN Member m ON m.id=MemberRights.created_by
     WHERE
@@ -11,4 +11,4 @@ $s="SELECT Concat(Member.FirstName,' ',Member.LastName) as roer, Member.MemberID
     ORDER BY MemberRight,tildelt,roer";
 
 $result=$rodb->query($s) or dbErr($rodb,$res,"q");
-process($result,"xlsx","årets_nye_rettigheder","_auto");
+process($result,"xlsx","årets_nye_rettigheder_$year","_auto");
