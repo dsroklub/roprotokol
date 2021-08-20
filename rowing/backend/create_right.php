6a422d3010f1a30a95a92ac8b6685d8a638c25a1
Niels Elgaard Larsen <elgaard@agol.dk>
2021-08-20 14:34:19 +0200

    allow new rights without arg

--- rowing/backend/create_right.php
+++ rowing/backend/create_right.php
@@ -6,9 +6,9 @@ include("inc/verify_user.php");
 $sjondata = file_get_contents("php://input");
 $right=json_decode($sjondata);
 error_log("new right: ".json_encode($right));
-
+$arg=$right->arg??"";
 $stmt = $rodb->prepare("INSERT INTO MemberRightType (member_right,description,arg,showname,predicate,active,category,validity) VALUES (?,?,?,?,?,?,?,?)") or dbErr($rodb,$res,"create right P");
-$stmt->bind_param('sssssisi', $right->member_right,$right->description,$right->arg,$right->showname,$right->predicate,$right->active,$right->category,$right->validity) || dbErr($rodb,$res,"create right bind");
+$stmt->bind_param('sssssisi', $right->member_right,$right->description,$arg,$right->showname,$right->predicate,$right->active,$right->category,$right->validity) || dbErr($rodb,$res,"create right bind");
 $stmt->execute() || dbErr($rodb,$res,"create right exe");
 $rodb->commit();
 $rodb->close();
