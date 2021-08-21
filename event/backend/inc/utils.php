d38711c450bfb7336e89c0f6fe9e0d2f88cf0505
Niels Elgaard Larsen <elgaard@agol.dk>
2021-08-21 12:59:32 +0200

    verify rights

--- event/backend/inc/utils.php
+++ event/backend/inc/utils.php
@@ -57,3 +57,26 @@ function saneEmail($s) {
     }
     return $r;
 }
+
+function verify_right($required,$arg=null) {
+// one of
+    global $rodb;
+    global $res;
+    if (!isset($_SERVER['PHP_AUTH_USER'])) {
+        return false;
+    }
+    $cuser=$_SERVER['PHP_AUTH_USER'];
+    if ($arg) {
+        $stmt=$rodb->prepare("SELECT 'x' FROM Member,MemberRights WHERE Member.MemberId=? AND MemberRights.member_id=Member.id AND MemberRight=? AND argument=?") or dbErr($rodb,$res,"verify right");
+        $stmt->bind_param("sss", $cuser,$required,$arg) or dbErr($rodb,$res,"owner verify bind");
+    } else {
+        $stmt=$rodb->prepare("SELECT 'x' FROM Member,MemberRights WHERE Member.MemberId=? AND MemberRights.member_id=Member.id AND MemberRight=?") or dbErr($rodb,$res,"verify right");
+        $stmt->bind_param("ss", $cuser,$required) or dbErr($rodb,$res,"owner verify bind");
+    }
+    $stmt->execute() or dbErr($rodb,$res,"right verify");
+    $result= $stmt->get_result() or dbErr($rodb,$res,"right verify res");
+    if ($result->num_rows<1) {
+        roErr("medlem $cuser har ikke rettigheden $required ".$arg??"");
+    }
+}
+
