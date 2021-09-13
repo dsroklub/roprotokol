794330cc3437a26433e96b8a5d9f4bdd2841e881
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-13 21:30:01 +0200

    Duplicate rowing/backend/inc/db.php history in event/backend/inc/db.php history.

---
+++ event/backend/inc/db.php
@@ -0,0 +1,17 @@
+<?php
+#error_log($_SERVER['DOCUMENT_ROOT'].'/../config.ini');
+$config = parse_ini_file(dirname($_SERVER['DOCUMENT_ROOT']).'/config.ini');
+$rodb=new mysqli("localhost",$config["dbuser"],$config["dbpassword"],$config["database"]);
+$adminpw=$config["adminpassword"];
+
+if (defined('MYSQLI_OPT_INT_AND_FLOAT_NATIVE')) {
+    $rodb->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
+}
+if ($rodb->connect_errno) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+if (!$rodb->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $rodb->error);
+}
