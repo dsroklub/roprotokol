0c75e9274766562bbd7164ee0cda64f645b130ea
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-13 21:45:22 +0200

    Duplicate rowing/backend/get_events.php history in event/backend/get_row_events.php history.

---
+++ event/backend/get_row_events.php
@@ -0,0 +1,14 @@
+<?php
+require("inc/common.php");
+include("inc/utils.php");
+
+$s="SELECT event, event_time FROM event_log ORDER BY event_time DESC LIMIT 500";
+$result=$rodb->query($s) or die("Error in event query: " . mysqli_error($rodb));;
+echo '[';
+ $first=1;
+ while ($row = $result->fetch_assoc()) {
+	  if ($first) $first=0; else echo ',';
+	  echo json_encode($row);
+}
+echo ']';
+$rodb->close();
