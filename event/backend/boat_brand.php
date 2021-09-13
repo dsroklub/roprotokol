9e9347a99536b70172d9cadf8d2fd0eac163730d
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-13 22:15:31 +0200

    Duplicate rowing/backend/boat_brand.php history in event/backend/ history.

---
+++ event/backend/boat_brand.php
@@ -0,0 +1,14 @@
+<?php
+require("inc/common.php");
+include("inc/utils.php");
+
+$s="SELECT id,name FROM boat_brand ORDER by name";
+$result=$rodb->query($s) or die("Error in stat query: " . mysqli_error($rodb));;
+echo '[';
+ $first=1;
+ while ($row = $result->fetch_assoc()) {
+	  if ($first) $first=0; else echo ',';	  
+	  echo json_encode($row);
+}
+echo ']';
+$rodb->close();
