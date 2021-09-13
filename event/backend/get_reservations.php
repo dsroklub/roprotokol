484f66fbd5a1502a690306f66565044868097333
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-13 22:21:58 +0200

    Duplicate rowing/backend/get_reservations.php history in event/backend/ history.

---
+++ event/backend/get_reservations.php
@@ -0,0 +1,20 @@
+<?php
+require("inc/common.php");
+include("inc/utils.php");
+
+$s="
+SELECT reservation.id,Boat.id as boat_id,Boat.Name as boat, TIME_FORMAT(start_time,'%H:%i') as start_time,start_date,TIME_FORMAT(end_time,'%H:%i') AS end_time,end_date,
+    dayofweek,reservation.description,TripType.Name as triptype, TripType.id as triptype_id,purpose, configuration
+    FROM reservation,Boat,TripType,reservation_configuration
+    WHERE Boat.id=reservation.boat AND TripType.id=reservation.triptype AND
+          (dayofweek>0 OR end_date>=DATE(NOW())) AND
+          reservation_configuration.name=reservation.configuration
+    ORDER BY boat,start_date,dayofweek,start_time
+";
+
+if ($sqldebug) {
+    echo $s;
+}
+$result=$rodb->query($s) or dbErr($rodb,$res,"GET res");
+output_rows($result);
+$rodb->close();
