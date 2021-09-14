372142e9efa6404459c30f9da28c53c440a62c0d
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 22:55:32 +0200

    Duplicate rowing/backend/set_boat_note.php history in event/backend/boats history.

---
+++ event/backend/boats/set_boat_note.php
@@ -0,0 +1,15 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+
+$data = file_get_contents("php://input");
+$data=json_decode($data);
+$rodb->begin_transaction();
+
+$stmt = $rodb->prepare("UPDATE Boat set note=? Where id=?") or dbErr($rodb,$res,'boat note');
+$stmt->bind_param('si', $data->note,$data->id) or dbErr($rodb,$res,'boat note bind');
+$stmt->execute() or dbErr($rodb,$res,'boat note exe');
+$rodb->commit();
+$rodb->close();
+invalidate('boat');
+echo json_encode($res);
--- event/backend/boats/set_side_for_boat.php
+++ event/backend/boats/set_side_for_boat.php
@@ -1,19 +1,16 @@
 <?php
 include("inc/common.php");
-include("inc/verify_user.php");
-
+$vr=verify_right(["admin"=>"roprotokol","admin"=>"boat"]);
 $error=null;
 $res=array ("status" => "ok");
 $data = file_get_contents("php://input");
 $data=json_decode($data);
 error_log("set side ".json_encode($data));
-
 $rodb->begin_transaction();
-
-if ($stmt = $rodb->prepare("UPDATE Boat set placement_side=? Where id=?")) { 
+if ($stmt = $rodb->prepare("UPDATE Boat set placement_side=? Where id=?")) {
     $stmt->bind_param('si', $data->placement_side,$data->id);
     $stmt->execute();
-} 
+}
 $rodb->commit();
 $rodb->close();
 invalidate('boat');
