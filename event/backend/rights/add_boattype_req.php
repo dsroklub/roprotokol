6b496a87031d7993a0ff93828a4a445c512d6933
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 23:03:43 +0200

    Duplicate rowing/backend/add_triptype_req.php history in event/backend/rights/ history.

--- event/backend/rights/add_boattype_req.php
+++ event/backend/rights/add_boattype_req.php
@@ -1,16 +1,12 @@
 <?php
-include("inc/common.php");
-include("inc/verify_user.php");
-
+include("../inc/common.php");
+$vr=verify_right(["admin"=>"roprotokol","admin"=>"boat","admin"=>"right"]);
 $error=null;
 $res=array ("status" => "ok");
 $data = file_get_contents("php://input");
 $data=json_decode($data);
-
-
 $rodb->begin_transaction(); // 
 error_log('add right '.json_encode($data));
-
 $stmt = $rodb->prepare  ("INSERT INTO BoatRights (boat_type,required_right,requirement ) VALUES (?,?,?)") or dbErr($rodb,$res,"BT req (prep)");
 $stmt->bind_param('sss', $data->boat_type->name,$data->right,$data->subject) or dbErr($rodb,$res,"BT req (bind)");
 $stmt->execute() or dbErr($rodb,$res,"BT req");
---
+++ event/backend/rights/add_triptype_req.php
@@ -0,0 +1,15 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+$res=array ("status" => "ok");
+$data = file_get_contents("php://input");
+$data=json_decode($data);
+$rodb->begin_transaction();
+//error_log('add trip right '.json_encode($data));
+$stmt = $rodb->prepare("INSERT INTO  TripRights (trip_type,required_right,requirement ) VALUES (?,?,?)") or dbErr($rodb,$res,"create trip req");
+$stmt->bind_param('iss', $data->triptype->id,$data->right,$data->subject) || dbErr($rodb,$res,"create trip req b");
+$stmt->execute() || dbErr($rodb,$res,"create trip req X");
+$rodb->commit();
+$rodb->close();
+invalidate('trip');
+echo json_encode($res);
