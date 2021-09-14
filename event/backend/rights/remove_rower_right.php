a28660c92a86d3283795b0a408acd36af01f1525
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 23:00:01 +0200

    Duplicate rowing/backend/remove_rower_right.php history in event/backend/rights/ history.

---
+++ event/backend/rights/remove_rower_right.php
@@ -0,0 +1,31 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+
+$error=null;
+$res=array ("status" => "ok");
+$data = file_get_contents("php://input");
+$data=json_decode($data);
+
+$admin="bådhalsbruger";
+if (!empty($cuser)) {
+    $admin=$cuser;
+}
+
+$rodb->begin_transaction();
+error_log('delete rower right:  '.json_encode($data));
+
+error_log('id='.$data->rower->id);
+error_log('right'.$data->right->member_right." -".$data->right->arg);
+
+if ($stmt = $rodb->prepare("DELETE FROM MemberRights WHERE MemberRight=? AND (argument IS NULL OR argument=?) AND member_id IN (SELECT id FROM Member WHERE MemberID=?)")) {
+    $stmt->bind_param('sss', $data->right->member_right,$data->right->arg,$data->rower->id);
+    $stmt->execute();
+} else {
+    error_log('OOOP'.$rodb->error);
+}
+$rodb->commit();
+eventLog("$admin fjernede ".$data->right->member_right ." fra ".$data->rower->id);
+$rodb->close();
+invalidate('member');
+echo json_encode($res);
