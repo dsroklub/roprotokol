23aa8859d4a89deb058305e2716af14755f60413
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 22:14:02 +0200

    Duplicate rowing/backend/boatreservations.php history in event/backend/reservations/ history.

---
+++ event/backend/reservations/boatreservations.php
@@ -0,0 +1,19 @@
+<?php
+include("inc/common.php");
+header('Content-type: application/json');
+
+$s="SELECT id, Boat as boat_id, Begin as start, End as end, Description as description FROM Reservation WHERE End > Now()";
+// for debug    $s="SELECT ID as id, FK_BådID as boat_id, start, slut as end, Beskrivelse as description FROM Reservation WHERE slut > '2013-08-30'";
+
+
+// echo $s;
+$result=$rodb->query($s) or die("Error in stat query: " . mysqli_error($rodb));
+echo '[';
+ $first=1;
+ while ($row = $result->fetch_assoc()) {
+	  if ($first) $first=0; else echo ',';	  
+	  echo json_encode($row);
+}
+echo ']';
+$rodb->close();
+invalidate("boat");
