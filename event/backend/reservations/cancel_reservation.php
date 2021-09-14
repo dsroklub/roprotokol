71de518b2a9d4360629bd16dd4edfa2bbb8b2317
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 22:13:47 +0200

    Duplicate rowing/backend/cancel_reservation.php history in event/backend/reservations/ history.

---
+++ event/backend/reservations/cancel_reservation.php
@@ -0,0 +1,17 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+
+$res=array ("status" => "ok");
+$data = file_get_contents("php://input");
+$data=json_decode($data);
+
+error_log("cancel res ".print_r($data,true));
+$rodb->begin_transaction();
+$stmt = $rodb->prepare("DELETE FROM reservation  WHERE id=?")  or dbErr($rodb,$res,"deletes reservation (Exe)");
+$stmt->bind_param("i", $data->id) or dbErr($rodb,$res,"delete reservation (Bind)");
+$stmt->execute()  or dbErr($rodb,$res,"deletes reservation (Exe c)");
+$rodb->commit();
+$rodb->close();
+invalidate("reservation");
+echo json_encode($res);
