fe0ed2252a9e2beafee8bcb8c2068c4dc8cd9fea
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 22:53:41 +0200

    Duplicate rowing/backend/set_level_for_boat.php history in event/backend/boats/ history.

---
+++ event/backend/boats/set_level_for_boat.php
@@ -0,0 +1,21 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+
+
+$error=null;
+$res=array ("status" => "ok");
+$data = file_get_contents("php://input");
+$data=json_decode($data);
+error_log("set level ".json_encode($data));
+
+$rodb->begin_transaction();
+
+if ($stmt = $rodb->prepare("UPDATE Boat set placement_level=? Where id=?")) { 
+    $stmt->bind_param('ii', $data->placement_level,$data->id);
+    $stmt->execute();
+} 
+$rodb->commit();
+$rodb->close();
+invalidate('boat');
+echo json_encode($res);
--- event/backend/boats/set_row_for_boat.php
+++ event/backend/boats/set_row_for_boat.php
@@ -1,15 +1,12 @@
 <?php
-include("inc/common.php");
-include("inc/verify_user.php");
+include("../inc/common.php");
 
 $error=null;
 $res=array ("status" => "ok");
 $data = file_get_contents("php://input");
 $data=json_decode($data);
 error_log("set row ".json_encode($data));
-
 $rodb->begin_transaction();
-
 if ($stmt = $rodb->prepare("UPDATE Boat set placement_row=? Where id=?")) {
     $stmt->bind_param('ii', $data->placement_row,$data->id);
     $stmt->execute();
