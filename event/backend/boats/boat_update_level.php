0d0024eb6ad1b81cee42aa2e44de9b7ca9f73bf2
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 22:44:09 +0200

    Duplicate rowing/backend/usage_update_name.php history in event/backend/boats/ history.

--- event/backend/boats/boat_update_level.php
+++ event/backend/boats/boat_update_level.php
@@ -1,6 +1,6 @@
 <?php
-include("inc/common.php");
-include("inc/verify_user.php");
+include("../inc/common.php");
+$vr=verify_right(["admin"=>"roprotokol","admin"=>"boat"]);
 
 $error=null;
 $res=array ("status" => "ok");
---
+++ event/backend/boats/usage_update_name.php
@@ -0,0 +1,20 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+
+$error=null;
+$res=array ("status" => "ok");
+$data = file_get_contents("php://input");
+$usage=json_decode($data);
+
+$rodb->begin_transaction();
+if ($stmt = $rodb->prepare("UPDATE boat_usage SET name=? WHERE id=?")) {
+    $stmt->bind_param('si', $usage->name,$usage->id);
+    $stmt->execute() |  error_log("usage update name :".$rodb->error);
+} else {
+    error_log("usage update nm error :".$rodb->error);
+}
+$rodb->commit();
+$rodb->close();
+invalidate('boat');
+echo json_encode($res);
