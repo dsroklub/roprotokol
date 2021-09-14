13b91fe2d5aedf6e4ab69c26fb59f9ffae12c66f
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 22:37:37 +0200

    Duplicate rowing/backend/add_boattype_req.php history in event/backend/boats/ history.

---
+++ event/backend/boats/add_boattype_req.php
@@ -0,0 +1,21 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+
+$error=null;
+$res=array ("status" => "ok");
+$data = file_get_contents("php://input");
+$data=json_decode($data);
+
+
+$rodb->begin_transaction(); // 
+error_log('add right '.json_encode($data));
+
+$stmt = $rodb->prepare  ("INSERT INTO BoatRights (boat_type,required_right,requirement ) VALUES (?,?,?)") or dbErr($rodb,$res,"BT req (prep)");
+$stmt->bind_param('sss', $data->boat_type->name,$data->right,$data->subject) or dbErr($rodb,$res,"BT req (bind)");
+$stmt->execute() or dbErr($rodb,$res,"BT req");
+$rodb->commit();
+$rodb->close();
+invalidate('boat');
+invalidate('member');
+echo json_encode($res);
