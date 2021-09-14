becd34f83bfb1841ee77d2e3b871be26dadab29c
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 22:57:50 +0200

    Duplicate rowing/backend/unretire_boat.php history in event/backend/boats history.

--- event/backend/boats/retire_boat.php
+++ event/backend/boats/retire_boat.php
@@ -1,13 +1,10 @@
 <?php
-include("inc/common.php");
-include("inc/verify_user.php");
-
+include("../inc/common.php");
+$vr=verify_right(["admin"=>"roprotokol","admin"=>"boat"]);
 $data = file_get_contents("php://input");
 $data=json_decode($data);
-
 $rodb->begin_transaction();
 error_log("retire boat ".json_encode($data));
-
 if ($stmt = $rodb->prepare("UPDATE Boat SET Decommissioned=NOW(),Location=NULL,placement_aisle=NULL,placement_level=NULL,placement_row=NULL,placement_side=NULL WHERE Name=?")) {
     $stmt->bind_param('s', $data->name);
     $stmt->execute();
