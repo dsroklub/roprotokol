fdacbc088ead2cf44c39551dd960d9841a23cee0
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 22:51:38 +0200

    Duplicate rowing/backend/set_aisle_for_boat.php history in event/backend/boats/ history.

---
+++ event/backend/boats/set_aisle_for_boat.php
@@ -0,0 +1,19 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+
+$error=null;
+$res=array ("status" => "ok");
+$data = file_get_contents("php://input");
+$data=json_decode($data);
+
+$rodb->begin_transaction();
+
+if ($stmt = $rodb->prepare("UPDATE Boat set placement_aisle=? Where id=?")) {
+    $stmt->bind_param('ii', $data->placement_aisle,$data->id);
+    $stmt->execute();
+}
+$rodb->commit();
+$rodb->close();
+invalidate('boat');
+echo json_encode($res);
