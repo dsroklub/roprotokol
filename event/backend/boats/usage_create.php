01ec44a675dc68daacebb91ee35c1c0362e50262
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 22:44:26 +0200

    Duplicate rowing/backend/usage_create.php history in event/backend/boats/ history.

---
+++ event/backend/boats/usage_create.php
@@ -0,0 +1,29 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+
+$error=null;
+$res=array ("status" => "ok");
+$data = file_get_contents("php://input");
+$usage=json_decode($data);
+error_log("new usage ".$data);
+$newusage=array("name"=>$usage->name,"description"=>$usage->description);
+$rodb->begin_transaction();
+if ($stmt = $rodb->prepare("INSERT INTO boat_usage(name,Description) VALUES (?,?)")) {
+    $stmt->bind_param('ss', $usage->name,$usage->description);
+    $stmt->execute() |  error_log("usage create exe error :".$rodb->error);
+
+    $result=$rodb->query("SELECT LAST_INSERT_ID() AS id FROM DUAL") or die("Error in new id query: " . mysqli_error($rodb));
+    $nid = $result->fetch_assoc();
+    if ($nid) {
+        $newid=$nid["id"];
+        $newusage["id"]=$newid;
+        $res["newusage"]=$newusage;
+    }
+} else {
+    error_log("usage create nm error :".$rodb->error);
+}
+$rodb->commit();
+$rodb->close();
+invalidate("boat");
+echo json_encode($res);
