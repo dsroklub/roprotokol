416fec4d8570beaac7987412cc507254b387d074
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 22:35:20 +0200

    Duplicate rowing/backend/activate_triptype.php history in event/backend/ history.

---
+++ event/backend/activate_triptype.php
@@ -0,0 +1,18 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+
+$error=null;
+$res=array ("status" => "ok");
+$data = file_get_contents("php://input");
+$data=json_decode($data);
+
+$rodb->begin_transaction();
+if ($stmt = $rodb->prepare("UPDATE TripType SET Active=? WHERE id=?")) {
+    $stmt->bind_param('ii', $data->active,$data->id);
+    $stmt->execute();
+}
+$rodb->commit();
+$rodb->close();
+invalidate('trip');
+echo json_encode($res);
--- event/backend/convert_rower.php
+++ event/backend/convert_rower.php
@@ -1,6 +1,6 @@
 <?php
 include("inc/common.php");
-include("inc/verify_user.php");
+$vr=verify_right(["admin"=>"roprotokol","admin"=>"rower"]);
 $res=array ("status" => "ok");
 
 $data = file_get_contents("php://input");
