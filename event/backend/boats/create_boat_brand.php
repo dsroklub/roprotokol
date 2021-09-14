3dd71b6417932d10c20f81997bce98acdf551aa2
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 22:46:58 +0200

    Duplicate rowing/backend/create_boat_brand.php history in event/backend/boats/ history.

---
+++ event/backend/boats/create_boat_brand.php
@@ -0,0 +1,22 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+
+$error=null;
+$res=array ("status" => "ok");
+$data = file_get_contents("php://input");
+$data=json_decode($data);
+
+$location = $data->location;
+$rodb->begin_transaction();
+error_log("new bt ".json_encode($data));
+
+if ($stmt = $rodb->prepare("INSERT INTO boat_brand (name) VALUES (?)")) { 
+    $stmt->bind_param('s', $data->name);
+    $stmt->execute();
+} 
+$rodb->commit();
+$rodb->close();
+invalidate('boat');
+invalidate('admin');
+echo json_encode($res);
