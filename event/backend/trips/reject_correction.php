7fcd186a522371caa7f89aa961577f5e4ea4357a
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 23:09:35 +0200

    Duplicate rowing/backend/reject_correction.php history in event/backend/trips history.

---
+++ event/backend/trips/reject_correction.php
@@ -0,0 +1,22 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+
+$error=null;
+$res=array ("status" => "ok");
+$data = file_get_contents("php://input");
+$data=json_decode($data);
+
+$rodb->begin_transaction();
+error_log('reject correction:  '.json_encode($data));
+
+if ($stmt = $rodb->prepare("UPDATE Error_Trip SET Fixed=2 WHERE id=?")) {
+    $stmt->bind_param('i', $data->correction->id);
+    $stmt->execute() || error_log(' error report rejection failed: '.$rodb->error);
+} else {
+    error_log('OOOP'.$rodb->error);
+}
+$rodb->commit();
+$rodb->close();
+invalidate('trip');
+echo json_encode($res);
