e85fee80ec57ee8c17a94943867856be681663f1
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 22:49:35 +0200

    Duplicate rowing/backend/set_cat_for_boat.php history in event/backend/boats/ history.

---
+++ event/backend/boats/set_cat_for_boat.php
@@ -0,0 +1,19 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+
+$error=null;
+$res=array ("status" => "ok");
+$data = file_get_contents("php://input");
+$data=json_decode($data);
+
+$location = $data->location;
+$rodb->begin_transaction();
+
+$stmt = $rodb->prepare("UPDATE Boat set boat_type=? WHERE id=?") or dbErr($rodb,$res,"set cat for boat (Prepare)");
+$stmt->bind_param('ss', $data->category,$data->id) or dbErr($rodb,$res,"set cat for boat (bind)");
+$stmt->execute() or dbErr($rodb,$res,"set cat for boat");
+$rodb->commit();
+$rodb->close();
+invalidate('boat');
+echo json_encode($res);
