36f6ed24f96e3ff390c739f1ee6cf85ae0df7c04
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-14 23:04:31 +0200

    Duplicate rowing/backend/remove_triptype_req.php history in event/backend/rights/ history.

---
+++ event/backend/rights/remove_triptype_req.php
@@ -0,0 +1,23 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+
+$error=null;
+$res=array ("status" => "ok");
+$data = file_get_contents("php://input");
+$data=json_decode($data);
+
+
+$rodb->begin_transaction();
+error_log('remove right '.json_encode($data));
+
+if ($stmt = $rodb->prepare("DELETE FROM TripRights WHERE trip_type=? AND required_right=? AND requirement=?")) {
+    $stmt->bind_param('iss', $data->triptype->id,$data->right->required_right,$data->right->requirement);
+    $stmt->execute();
+} else {
+    error_log('OOOP'.$rodb->error);
+}
+$rodb->commit();
+$rodb->close();
+invalidate('trip');
+echo json_encode($res);
