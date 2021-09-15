fb311aa950e0fd12a6e39078a0db9cdca2ffb4cc
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-15 14:40:49 +0200

    Duplicate rowing/backend/create_destination.php history in event/backend/trips/ history.

--- event/backend/trips/set_zone.php
+++ event/backend/trips/set_zone.php
@@ -1,10 +1,8 @@
 <?php
-include("inc/common.php");
-include("inc/verify_user.php");
-
-$error=null;
+include("../inc/common.php");
+include("../inc/utils.php");
+$vr=verify_right(["admin"=>["roprotokol","trip"]]);
 $data = json_decode(file_get_contents("php://input"));
-
 $stmt = $rodb->prepare("UPDATE Destination SET Zone=? WHERE Name=? AND Location=?") or dbErr($rodb,$res,"cannot set zone (Prepare)");
 $stmt->bind_param('sss', $data->zone,$data->name,$data->location) || dbErr($rodb,$res,"cannot set zone (bind)");
 $stmt->execute() || dbErr($rodb,$res,"cannot set zone");
