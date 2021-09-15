8677dcd9f20618dc2df091f8e255dbc79c2f183f
Niels Elgaard Larsen <elgaard@agol.dk>
2021-09-15 14:37:24 +0200

    Duplicate rowing/backend/set_duration.php history in event/backend/trips/ history.

---
+++ event/backend/trips/set_duration.php
@@ -0,0 +1,19 @@
+<?php
+include("inc/common.php");
+include("inc/verify_user.php");
+
+$error=null;
+$res=array ("status" => "ok");
+$data = file_get_contents("php://input");
+$data=json_decode($data);
+
+$location = $data->location;
+$rodb->begin_transaction();
+($stmt = $rodb->prepare("UPDATE Destination SET ExpectedDurationNormal=?, ExpectedDurationInstruction=? WHERE Name=? AND Location=?")) || dbErr($rodb,$res,"cannot set duration (Prepare)");
+$stmt->bind_param('ddss', $data->duration,$data->duration_instruction ,$data->name,$data->location) || dbErr($rodb,$res,"cannot set duration (bind)");
+$stmt->execute() || dbErr($rodb,$res,"cannot set duration");
+$rodb->commit();
+$rodb->close();
+invalidate('destination');
+invalidate('boat');
+echo json_encode($res);
--- rowingapp/frontend/app/components/utilities/righttodk.js
+++
@@ -1,6 +0,0 @@
-angular.module('rowApp.utilities.righttodk', []).filter('righttodk', ['DatabaseService', function (db_service) {
-  return function (sb) {
-    var r=db_service.getRight2dk(sb);
-    return (r?r:sb);
-  };
-}]);
