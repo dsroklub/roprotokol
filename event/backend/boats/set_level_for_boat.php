ecfe3755c676eeda176f6dfd515da85c5add81ba
Niels Elgaard Larsen <elgaard@agol.dk>
2022-01-18 18:48:37 +0100

    set level without auth

--- event/backend/boats/set_level_for_boat.php
+++ event/backend/boats/set_level_for_boat.php
@@ -1,17 +1,16 @@
 <?php
 include("../inc/common.php");
 include("../inc/utils.php");
-$vr=verify_right(["admin"=>["roprotokol","boat"]]);
+//$vr=verify_right(["admin"=>["roprotokol","boat"]]);
 $error=null;
 $res=array ("status" => "ok");
 $data = file_get_contents("php://input");
 $data=json_decode($data);
-error_log("set level ".json_encode($data));
+error_log("$cuser set level ".json_encode($data));
 $rodb->begin_transaction();
-if ($stmt = $rodb->prepare("UPDATE Boat set placement_level=? Where id=?")) {
-    $stmt->bind_param('ii', $data->placement_level,$data->id);
-    $stmt->execute();
-}
+$stmt = $rodb->prepare("UPDATE Boat set placement_level=? WHERE id=?") or dbErr($rodb,$res,"could not set level");
+$stmt->bind_param('ii', $data->placement_level,$data->id) || dbErr($rodb,$res,"could not set level bind");
+$stmt->execute()  || dbErr($rodb,$res,"could not set level exe");
 $rodb->commit();
 $rodb->close();
 invalidate('boat');
