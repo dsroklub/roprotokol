b750a8a028da559de6eb2eca5dc9a6ce65a8238f
Niels Elgaard Larsen in DSR <elgaard@agol.dk>
2022-06-02 19:01:23 +0200

    use pw

--- utils/dbdump.sh
+++ utils/dbdump.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pw=$(grep dbpassword /data/config.ini|cut -d = -f 2)
-mysqldump roprotokol -u roprotokol -p > /data/backup/ro.sql
+mysqldump roprotokol -u roprotokol -p --password="$pw" > /data/backup/ro.sql
 scp /data/backup/ro.sql ro0:
