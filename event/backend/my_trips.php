6d54d527cc4c765b3b9395aaccc9e159249d4df3
Niels Elgaard Larsen in DSR <elgaard@agol.dk>
2022-06-08 19:54:44 +0200

    add headers to stat

--- event/backend/my_trips.php
+++ event/backend/my_trips.php
@@ -6,12 +6,12 @@ if (isset($_SERVER['PHP_AUTH_USER'])) {
     $cuser=$_SERVER['PHP_AUTH_USER'];
     $format=$_GET["output"] ?? "csv";
 
-    $s="SELECT OutTime as tid,Boat.name as boat,Trip.Meter/1000 as km ,Trip.Destination as destination
+    $s="SELECT OutTime as tid,Boat.name as båd,Trip.Meter/1000 as km ,Trip.Destination as destination
     FROM Trip,TripMember,Member,Boat
     WHERE TripMember.TripID=Trip.id AND Member.id=TripMember.member_id AND Member.MemberID=?
        AND Boat.id=Trip.BoatId
 UNION
-   SELECT start_time as tid, team as boat, 0 as km, '' as destination
+   SELECT start_time as tid, team as båd, 0 as km, '' as destination
    FROM team_participation, Member
    WHERE team_participation.member_id=Member.id AND Member.MemberId=?
 ORDER BY tid DESC";
@@ -20,7 +20,7 @@ ORDER BY tid DESC";
     $stmt->bind_param("ss",$cuser,$cuser) || dbErr($rodb,$res,"mytrips");
     $stmt->execute() || dbErr($rodb,$res,"mytrips exe");
     $result= $stmt->get_result();
-    process($result,$output,"${cuser}_roture");
+    process($result,$output,"${cuser}_roture","_auto");
     $stmt->close();
 } else {
     echo "No user";
