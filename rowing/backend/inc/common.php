13d75ecc250a5cfb9175487444924149fef71d21
Niels Elgaard Larsen in DSR <elgaard@agol.dk>
2022-08-09 03:32:39 +0200

    gym still uses office

--- rowing/backend/inc/common.php
+++ rowing/backend/inc/common.php
@@ -1,10 +1,10 @@
 <?php
-// require __DIR__.'/../vendor/autoload.php';
-// use PhpOffice\PhpSpreadsheet\Spreadsheet;
-// use PhpOffice\PhpSpreadsheet\Cell\DataType;
-// use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-// use PhpOffice\PhpSpreadsheet\Writer\Ods;
-// use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+ require __DIR__.'/../vendor/autoload.php';
+ use PhpOffice\PhpSpreadsheet\Spreadsheet;
+ use PhpOffice\PhpSpreadsheet\Cell\DataType;
+ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+ use PhpOffice\PhpSpreadsheet\Writer\Ods;
+ use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
 header('Content-Type: application/json; charset=utf-8');
 ini_set('default_charset', 'utf-8');
@@ -104,6 +104,7 @@ function process ($result,$output="json",$name="csvfile",$captions=null) {
     ];
     $colTypes=[];
     $maxlengths=[];
+    error_log("process $output c=$captions");
     if ($captions=="_auto") {
         $captions=[];
         foreach ($result->fetch_fields() as $fi => $fl) {
@@ -144,6 +145,55 @@ function process ($result,$output="json",$name="csvfile",$captions=null) {
             $rn=$rn+1;
         }
         echo ']}';
+    } else if ($output=="ods" || $output=="xlsx") {
+        header('Content-Disposition: filename="'.$name.".$output".'"');
+        if ($output=="ods"){
+            header('Content-Type: application/vnd.oasis.opendocument.spreadsheet');
+        } else {
+            header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        }
+        //\PhpOffice\PhpSpreadsheet\Shared\Date::setDefaultTimezone("UTC");
+        header('Cache-Control: max-age=0');
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet()->setTitle($name);
+        $spreadsheet->getProperties()
+            ->setCreator('DSR roprotokol')
+            ->setTitle($name)
+            ->setSubject($name)
+            ->setDescription('DSR roprotokol rapport')
+            ->setKeywords('DSR roprotokol aftaler');
+        $ri=1;
+        if ($captions) {
+            foreach ($captions as $ci => $caption) {
+                $sheet->setCellValueByColumnAndRow($ci+1,1,"$caption");
+            }
+            $sheet->freezePane("A2");
+        }
+        foreach ($maxlengths as $ci => $cw) {
+            $spreadsheet->getActiveSheet()->getColumnDimensionByColumn($ci+1)->setWidth(min(80,max(5,$cw+1)));
+        }
+        $ri++;
+        while ($row = $result->fetch_assoc()) {
+            $ci=1;
+            foreach ($row as $cn=>$rc) {
+                if (isset($colTypes[$cn]) && isset($formatMap[$colTypes[$cn]])) {
+                    if ($colTypes[$cn]==MYSQLI_TYPE_DATETIME && $output=="xlsx")  {
+                        $rc=\PhpOffice\PhpSpreadsheet\Shared\Date::PHPToExcel(strtotime($rc."Z"));
+                        $dataType=DataType::TYPE_NUMERIC;
+                        $sheet->getStyleByColumnAndRow($ci,$ri)->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_DATE_YYYYMMDDSLASH);
+                    } else {
+                        $dataType=$formatMap[$colTypes[$cn]];
+                    }
+                } else {
+                    $dataType=DataType::TYPE_STRING;
+                }
+                $sheet->setCellValueExplicitByColumnAndRow($ci++,$ri,$rc,$dataType);
+
+            }
+            $ri++;
+        }
+        $writer = ($output=="xlsx")?new Xlsx($spreadsheet):new Ods($spreadsheet);
+        $writer->save('php://output');
     } else if ($output=="text") {
         header('Content-type: text/html');
         echo " <link rel=\"stylesheet\" href=\"/public/stat.css\">\n<table>\n";
