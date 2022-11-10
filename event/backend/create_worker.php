52a18bbd1c2a153a586ff4df134cf0f1e3b300fb
Niels Elgaard Larsen in DSR <elgaard@agol.dk>
2022-11-10 17:01:38 +0100

    set description for new worker

--- event/backend/create_worker.php
+++ event/backend/create_worker.php
@@ -4,8 +4,8 @@ require("utils.php");
 verify_right(["admin"=>["vedligehold"]]);
 $data = file_get_contents("php://input");
 $d=json_decode($data);
-$stmt=$rodb->prepare("INSERT INTO worker(member_id,assigner, created,created_by,requirement,workertype )
-  SELECT m.id,'vedligehold',NOW(),mby.id,?,? FROM Member m, Member mby WHERE m.MemberId=? AND mby.MemberId=?") or dbErr($rodb,$res,"set work");
+$stmt=$rodb->prepare("INSERT INTO worker(member_id,assigner, created,created_by,requirement,workertype,description )
+  SELECT m.id,'vedligehold',NOW(),mby.id,?,?,'vintervedligehold' FROM Member m, Member mby WHERE m.MemberId=? AND mby.MemberId=?") or dbErr($rodb,$res,"set work");
 $stmt->bind_param("dsss", $d->requirement,$d->workertype,$d->id,$cuser) || dbErr($rodb,$res,"create worker");
 $stmt->execute() or dbErr($rodb,$res,"create work exe");
 invalidate("work");
