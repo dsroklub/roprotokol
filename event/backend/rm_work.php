de6a3c9378847914b7479c46285cec3757fe2129
Niels Elgaard Larsen in DSR <elgaard@agol.dk>
2023-02-05 16:13:56 +0100

    work editing

--- event/backend/rm_work.php
+++ event/backend/rm_work.php
@@ -5,7 +5,7 @@ verify_right(["admin"=>["vedligehold"]]);
 error_log("rmwork cuser $cuser");
 $data = file_get_contents("php://input");
 $d=json_decode($data);
-$stmt=$rodb->prepare("DELETE FROM worklog WHERE id=? AND season=$workyear") or dbErr($rodb,$res,"rmwork prep");
+$stmt=$rodb->prepare("DELETE FROM worklog WHERE id=?") or dbErr($rodb,$res,"rmwork prep");
 $stmt->bind_param("s", $d->id) || dbErr($rodb,$res,"rmwork e");
 $stmt->execute() or dbErr($rodb,$res,"rmwork exe");
 invalidate("work");
--- event/backend/set_work_req.php
+++ event/backend/set_work_req.php
@@ -4,7 +4,7 @@ require("inc/utils.php");
 verify_right(["admin"=>["vedligehold"]]);
 $data = file_get_contents("php://input");
 $d=json_decode($data);
-$stmt=$rodb->prepare("UPDATE worker SET requirement=?,workertype=? WHERE season=$workyear (assigner='vedligehold' OR description='vintervedligehold') AND member_id IN (SELECT id FROM Member WHERE MemberID=?)") or dbErr($rodb,$res,"set work");
+$stmt=$rodb->prepare("UPDATE worker SET requirement=?,workertype=? WHERE season=$workyear AND (assigner='vedligehold' OR description='vintervedligehold') AND member_id IN (SELECT id FROM Member WHERE MemberID=?)") or dbErr($rodb,$res,"set work");
 $stmt->bind_param("dss", $d->worker->requirement,$d->worker->workertype,$d->worker->worker_id) || dbErr($rodb,$res,"set work b");
 $stmt->execute() or dbErr($rodb,$res,"set work exe");
 invalidate("work");
