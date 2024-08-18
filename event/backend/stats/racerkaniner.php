17f4b33af36b7690b4c7b498639280f49c2a4bc0
Niels Elgaard Larsen in DSR <elgaard@agol.dk>
2024-08-19 01:18:16 +0200

    name change

--- event/backend/stats/racerkaniner.php
+++ event/backend/stats/racerkaniner.php
@@ -4,8 +4,8 @@ include("../inc/utils.php");
 $vr=verify_right(["admin"=>[null],"data"=>["stat"]]);
 $s='SELECT MemberID as medlemsnummer,CONCAT(FirstName," ",LastName) as navn, ROUND(Sum(Meter)/1000,1) AS Distance, COUNT("x") AS gange
 FROM Trip,TripMember, Member, TripType
-WHERE Trip.id=TripMember.TripID AND TripType.id=Trip.TripTypeID AND Member.id=TripMember.member_id AND YEAR(OutTime)=YEAR(NOW()) AND TripType.Name="Førsteårsroning" Group By Member.id ORDER BY FirstName,LastName
+WHERE Trip.id=TripMember.TripID AND TripType.id=Trip.TripTypeID AND Member.id=TripMember.member_id AND YEAR(OutTime)=YEAR(NOW()) AND TripType.Name="Kaninholdet" Group By Member.id ORDER BY FirstName,LastName
 ';
 $result=$rodb->query($s) or dbErr($rodb,$res,"Error in racerkaniner query: " );
 $output='xlsx';
-process($result,$output,"førsteårsroere","_auto");
+process($result,$output,"kaninholdet","_auto");
