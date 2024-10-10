a4d1b216c1005ce067037ad96d1605393a0d49f7
Niels Elgaard Larsen in DSR <elgaard@agol.dk>
2024-10-10 12:39:20 +0200

    new stats

--- event/backend/stats/team.php
+++ event/backend/stats/team.php
@@ -49,7 +49,7 @@ $spreadsheet->getProperties()
 
 $teamIx=[];
 $teams=
-    "SELECT DISTINCT team_participation.team, teacher, IF(DAYOFWEEK(start_time)<2,7,DAYOFWEEK(start_time)-1) dayno FROM team_participation LEFT JOIN team ON team.name=team_participation.team WHERE YEAR(start_time)=? ORDER by dayno,team";
+    "SELECT DISTINCT team_participation.team, team_participation.timeofday,teacher, IF(DAYOFWEEK(start_time)<2,7,DAYOFWEEK(start_time)-1) dayno FROM team_participation LEFT JOIN team ON team.name=team_participation.team AND team.timeofday=team_participation.timeofday WHERE YEAR(start_time)=? ORDER by dayno,team,timeofday";
 $stmt=$rodb->prepare($teams) or dbErr($rodb,$res,"teams prep");
 $stmt->bind_param("i", $y) || dbErr($rodb,$res,"teams y bind");
 $stmt->execute() || dbErr($rodb,$res,"teams exe");
@@ -58,12 +58,13 @@ $teamresult=$stmt->get_result() or dbErr($rodb,$res,"Error in team name stats qu
 $maxweek=1;
 $col=2;
 foreach ($teamresult as $tr) {
-    $teamIx[$tr["dayno"]][$tr["team"]]=$col;
+    $teamIx[$tr["dayno"]][$tr["team"]][$tr["timeofday"]]=$col;
     $sheet->setCellValue([$col,2],$weekdays[$tr["dayno"]-1]);
     $sheet->setCellValue([$col,3],$tr["team"]);
     if (!empty($tr["teacher"])) {
         $sheet->setCellValue([$col,4],$tr["teacher"]);
     }
+    $sheet->setCellValue([$col,5],$tr["timeofday"]);
     $col++;
 }
 
@@ -73,7 +74,7 @@ $s=
 "SELECT COUNT('x') AS h, dayofweek,timeofday,team, week(start_time,3) w,IF(DAYOFWEEK(start_time)<2,7,DAYOFWEEK(start_time)-1) dayno
 FROM team_participation
 WHERE YEAR(start_time)=?
-GROUP BY w, team,dayofweek
+GROUP BY w, team,dayofweek,timeofday
 ORDER BY w,dayno
 ";
 $stmt=$rodb->prepare($s) or dbErr($rodb,$res,"prep");
@@ -90,18 +91,10 @@ $sheet->setCellValueExplicit([1,1],"Gymnastik $y",DataType::TYPE_STRING);
 $sheet->setCellValue([1,2],"Uge");
 $sheet->setCellValue([1,3],"hold");
 $sheet->setCellValue([1,4],"underviser");
+$sheet->setCellValue([1,5],"tid");
 
 
-$row=5;
-foreach ($result as $r) {
-    //    print_r($r);
-    $u[$r["w"]][$r["dayofweek"]]["team"]=$r['h'];
-    if ($maxweek<$r["w"]) {
-        $maxweek=$r["w"];
-    }
-}
-
-//print_r($u);
+$row=6;
 
 foreach (range(1,$maxweek+1) as $cw) {
     $wr=$row+$cw;
@@ -126,11 +119,11 @@ $sheet->setCellValue([1,$totalRow],"TOTAL");
 
 foreach ($result as $r) {
     $sheet->setCellValue([1,$row+$r['w']],$r["w"]);
-    $sheet->setCellValue([$teamIx[$r["dayno"]][$r["team"]],$row+$r['w']],$r["h"]);
+    $sheet->setCellValue([$teamIx[$r["dayno"]][$r["team"]][$r["timeofday"]],$row+$r['w']],$r["h"]);
 
 }
 
-$sheet->freezePane("B5");
+$sheet->freezePane("B6");
 
 $statoutput="xlsx";
 $writer = ($statoutput=="xlsx")?new Xlsx($spreadsheet):new Ods($spreadsheet);
